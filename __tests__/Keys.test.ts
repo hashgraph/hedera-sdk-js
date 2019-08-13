5d56d96634f8aa25e689e5a873ec20f26ef6bde4
Austin Bonander <austin@launchbadge.com>
2019-08-12 19:20:54 -0700

    fix Keys.test.js

--- __tests__/Keys.test.ts
+++ __tests__/Keys.test.ts
@@ -1,17 +1,9 @@
-/*
-FIXME: get this test working again
-import { encodeKey, decodeKeyPair } from '../src/Keys';
 
-const key = Uint8Array.of(
-    -37, 72, 75, -126, -114, 100, -78, -40, -15, 44, -29, -64, -96, -23, 58, 11, -116, -50,
-    122, -15, -69, -113, 57, -55, 119, 50, 57, 68, -126, 83, -114, 16);
+import { encodeKey, decodeKeyPair } from '../src/Keys';
 
 const keyStr = '302e020100300506032b657004220420db484b828e64b2d8f12ce3c0a0e93a0b8cce7af1bb8f39c97732394482538e10';
 
-test('encodeKey produces correctly encoded string', () => {
-    expect(encodeKey(key)).toEqual(keyStr);
+test('key encoding functions correctly', () => {
+    const { privateKey } = decodeKeyPair(keyStr);
+    expect(encodeKey(privateKey).toString('hex')).toEqual(keyStr);
 });
-
-test('decodeKey returns correct value', () => {
-    expect(decodeKeyPair(keyStr)).toEqual(key);
-});*/
