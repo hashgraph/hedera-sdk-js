944efccbd9591f6d82233f3b6ec90dcd705e8284
Austin Bonander <austin@launchbadge.com>
2019-08-14 14:35:34 -0700

    we don't need to explicitly refer to protoc-gen-ts

--- compile-protos.sh
+++ compile-protos.sh
@@ -1,18 +1,12 @@
 #!/bin/sh
 
-# Path to this plugin, Note this must be an abolsute path on Windows (see #15)
-PROTOC_GEN_TS_PATH="./node_modules/.bin/protoc-gen-ts"
-
 # Directory to write generated code to (.js and .d.ts files)
 OUT_DIR="./src/generated"
 
 rm -rf src/generated
 mkdir -p src/generated
 
-echo "current working dir: $PWD"
-
 protoc \
-    --plugin="protoc-gen-ts=${PROTOC_GEN_TS_PATH}" \
     --js_out="import_style=commonjs,binary:${OUT_DIR}" \
     --ts_out="service=true:${OUT_DIR}" \
     -I "src/proto" \
