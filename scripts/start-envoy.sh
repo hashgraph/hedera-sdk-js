c1e6ac8469787d7d6a72c8696ff108050d6af6e5
Austin Bonander <austin@launchbadge.com>
2019-08-22 17:32:29 -0700

    modify `start-envoy.sh` to only mount `envoy.yaml`

--- scripts/start-envoy.sh
+++ scripts/start-envoy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run -dt -v "$(pwd)":/etc/envoy:ro --name envoy -p 11205:11205 envoyproxy/envoy
+docker run -dt -v "$(pwd)"/envoy.yaml:/etc/envoy/envoy.yaml:ro --name envoy -p 11205:11205 envoyproxy/envoy
