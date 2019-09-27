132676d984ef226b08f83af26ebe545976efeef9
Daniel Akhterov <daniel@launchbadge.com>
2019-09-27 14:10:21 -0700

    Resolve review comments
    Signed-off-by: Daniel Akhterov <daniel@launchbadge.com>

--- src/types/TransferList.ts
+++ src/types/TransferList.ts
@@ -1,16 +1,6 @@
-import {AccountAmount, accountAmountToSdk} from "./AccountAmount";
-import {TransferList as ProtoTransferList} from "../generated/CryptoTransfer_pb";
+import { AccountAmount, accountAmountToSdk } from "./AccountAmount";
+import { TransferList as ProtoTransferList } from "../generated/CryptoTransfer_pb";
 
-export type TransferList = {
-    accountAmountsList: AccountAmount[];
-}
-
-export function transferListToSdk(transferList: ProtoTransferList): TransferList {
-    const accountAmountsList = transferList.getAccountamountsList().map((accountAmount) => {
-        return accountAmountToSdk(accountAmount);
-    });
-
-    return {
-        accountAmountsList
-    }
+export function transferListToSdk(transferList: ProtoTransferList): AccountAmount[] {
+    return transferList.getAccountamountsList().map((accountAmount) => accountAmountToSdk(accountAmount));
 }
