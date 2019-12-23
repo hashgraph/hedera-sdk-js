14b419ea8e1b89c4fa61beb5ae29e8798e8a25a3
Ryan Leckey <ryan@launchbadge.com>
2019-12-23 14:34:39 -0800

    AccountStakersQuery -> AccountAmount and AccountAmount.amount -> Hbar

--- src/account/AccountAmount.ts
+++ src/account/AccountAmount.ts
@@ -1,14 +1,15 @@
 import { AccountAmount as ProtoAccountAmount } from "../generated/CryptoTransfer_pb";
 import { AccountId } from "./AccountId";
+import { Hbar } from "../Hbar";
 
 export interface AccountAmount {
     accountId: AccountId;
-    amount: string;
+    amount: Hbar;
 }
 
 export function accountAmountToSdk(accountAmount: ProtoAccountAmount): AccountAmount {
     return {
         accountId: AccountId._fromProto(accountAmount.getAccountid()!),
-        amount: accountAmount.getAmount()
+        amount: Hbar.fromTinybar(accountAmount.getAmount())
     };
 }
