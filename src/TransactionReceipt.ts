c7195867826ede699cba28481b37e5aab5687bd7
Daniel Akhterov <daniel@launchbadge.com>
2019-12-31 13:56:33 -0800

    TransactionReceipt Parity
        - Convert `TransactionReceipt` into a class instead
          of an object type
        - Implement accessors `getAccountId`, `getFileId`, `getContractId`
          on `TransactionReceipt`
    
    Signed-off-by: Daniel Akhterov <daniel@launchbadge.com>

--- src/TransactionReceipt.ts
+++ src/TransactionReceipt.ts
@@ -5,26 +5,50 @@ import { FileId } from "./file/FileId";
 import { ExchangeRateSet, exchangeRateSetToSdk } from "./ExchangeRate";
 import { Status } from "./Status";
 
-export interface TransactionReceipt {
-    status: Status;
-    accountId: AccountId | null;
-    fileId: FileId | null;
-    contractId: ContractId | null;
-    exchangeRateSet: ExchangeRateSet | null;
-}
+export class TransactionReceipt {
+    public readonly status: Status;
+    private accountId: AccountId | null;
+    private fileId: FileId | null;
+    private contractId: ContractId | null;
+    public readonly exchangeRateSet: ExchangeRateSet | null;
 
-export function receiptToSdk(receipt: ProtoTransactionReceipt): TransactionReceipt {
-    return {
-        status: receipt.getStatus() as Status,
-        accountId: receipt.hasAccountid() ? AccountId._fromProto(receipt.getAccountid()!) : null,
-        fileId: receipt.hasFileid() ? FileId._fromProto(receipt.getFileid()!) : null,
+    private constructor(
+        status: Status,
+        accountId: AccountId | null,
+        fileId: FileId | null,
+        contractId: ContractId | null,
+        exchangeRateSet: ExchangeRateSet | null
+    ) {
+        this.status = status;
+        this.accountId = accountId;
+        this.fileId = fileId;
+        this.contractId = contractId;
+        this.exchangeRateSet = exchangeRateSet;
+    }
 
-        contractId: receipt.hasContractid() ?
-            ContractId._fromProto(receipt.getContractid()!) :
-            null,
+    public getAccountId(): AccountId {
+        return this.accountId!;
+    }
 
-        exchangeRateSet: receipt.hasExchangerate() ?
-            exchangeRateSetToSdk(receipt.getExchangerate()!) :
-            null
-    };
+    public getFileId(): FileId {
+        return this.fileId!;
+    }
+
+    public getContractId(): ContractId {
+        return this.contractId!;
+    }
+
+    public static fromProto(receipt: ProtoTransactionReceipt): TransactionReceipt {
+        return new TransactionReceipt(
+            receipt.getStatus(),
+            receipt.hasAccountid() ? AccountId._fromProto(receipt.getAccountid()!) : null,
+            receipt.hasFileid() ? FileId._fromProto(receipt.getFileid()!) : null,
+            receipt.hasContractid() ?
+                ContractId._fromProto(receipt.getContractid()!) :
+                null,
+            receipt.hasExchangerate() ?
+                exchangeRateSetToSdk(receipt.getExchangerate()!) :
+                null
+        );
+    }
 }
