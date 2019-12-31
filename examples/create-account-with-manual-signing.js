e5813044f44e37a38d47c76e77a4f494a60ab554
Daniel Akhterov <daniel@launchbadge.com>
2019-12-30 18:47:16 -0800

    Move manual signing example into its own file
    Signed-off-by: Daniel Akhterov <daniel@launchbadge.com>

--- examples/create-account-with-manual-signing.js
+++ examples/create-account-with-manual-signing.js
@@ -1,30 +1,27 @@
 const { Client, Ed25519PrivateKey, AccountCreateTransaction } = require("@hashgraph/sdk");
 
 async function main() {
-    const operatorPrivateKey = process.env.OPERATOR_KEY;
-    const operatorAccount = process.env.OPERATOR_ID;
-
-    if (operatorPrivateKey == null || operatorAccount == null) {
+    if (process.env.OPERATOR_KEY == null || process.env.OPERATOR_ID == null) {
         throw new Error("environment variables OPERATOR_KEY and OPERATOR_ID must be present");
     }
 
-    const client = new Client({
-        network: { "0.testnet.hedera.com:50211": "0.0.3" },
-        operator: {
-            account: operatorAccount,
-            privateKey: operatorPrivateKey
-        }
-    });
+    const operatorPrivateKey = Ed25519PrivateKey.fromString(process.env.OPERATOR_KEY);
+    const operatorAccount = process.env.OPERATOR_ID;
+
+    const client = new Client({ network: { "0.testnet.hedera.com:50211": "0.0.3" }});
 
     const privateKey = await Ed25519PrivateKey.generate();
 
-    console.log("Automatic signing example");
+    console.log("Manual signing example");
     console.log(`private = ${privateKey.toString()}`);
     console.log(`public = ${privateKey.publicKey.toString()}`);
 
     const transactionId = await new AccountCreateTransaction()
         .setKey(privateKey.publicKey)
         .setInitialBalance(0)
+        .setTransactionId(operatorAccount)
+        .build(client)
+        .sign(operatorPrivateKey)
         .execute(client);
 
     const transactionReceipt = await transactionId.getReceipt(client);
@@ -34,4 +31,3 @@ async function main() {
 }
 
 main();
-
