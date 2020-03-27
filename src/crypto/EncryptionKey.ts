581197f01b5f8ff863e423b63b09d8fdfb38c679
Daniel Akhterov <akhterovd@gmail.com>
2020-03-26 21:01:29 -0700

    Implement `EncryptionKey.encrypt()`
    Signed-off-by: Daniel Akhterov <akhterovd@gmail.com>

--- src/crypto/EncryptionKey.ts
+++ src/crypto/EncryptionKey.ts
@@ -1,101 +1,53 @@
 import * as crypto from "crypto";
 import * as nacl from "tweetnacl";
 import { Pbkdf2 } from "./Pbkdf2";
-import { BadKeyError } from "../errors/BadKeyError";
-import { EncryptedPrivateKeyInfo } from "./pkcs";
-import { decodeDer } from "./der";
-import { BadPemFileError } from "../errors/BadPemFileError";
-import { HashAlgorithm } from "./Hmac";
-import * as base64 from "../encoding/base64";
-import { Ed25519PrivateKey } from "./Ed25519PrivateKey";
-
-const beginPrivateKey = "-----BEGIN PRIVATE KEY-----\n";
-const endPrivateKey = "-----END PRIVATE KEY-----\n";
-
-const beginEncryptedPkey = "-----BEGIN ENCRYPTED PRIVATE KEY-----\n";
-const endEncryptedPkey = "-----END ENCRYPTED PRIVATE KEY-----\n";
+import { Hmac, HashAlgorithm } from "./Hmac";
+import * as utf8 from "@stablelib/utf8";
 
 const AES_128_CTR = "aes-128-ctr";
 
 export class EncryptionKey {
-    private cipher: crypto.Cipher;
+    private key: Uint8Array;
+    private passphrase: string;
 
     private static dkLen = 32;
     private static c = 262144;
     private static saltLen = 32;
 
-    private constructor(cipher: crypto.Cipher) {
-        this.cipher = cipher;
+    private constructor(passphrase: string, key: Uint8Array) {
+        this.passphrase = passphrase;
+        this.key = key;
     }
-
-    // public static async fromPem(pem: string, passphrase?: string): Promise<EncryptionKey> {
-    //     const beginTag = passphrase ? beginEncryptedPkey : beginPrivateKey;
-    //     const endTag = passphrase ? endEncryptedPkey : endPrivateKey;
-
-    //     const beginIndex = pem.indexOf(beginTag);
-    //     const endIndex = pem.indexOf(endTag);
-
-    //     if (beginIndex === -1 || endIndex === -1) {
-    //         throw new BadPemFileError();
-    //     }
-
-    //     const keyEncoded = pem.slice(beginIndex + beginTag.length, endIndex);
-
-    //     const keyStr = base64.decode(keyEncoded);
-
-    //     let key;
-
-    //     if (passphrase) {
-    //         let encrypted;
-
-    //         try {
-    //             encrypted = EncryptedPrivateKeyInfo.parse(keyStr);
-    //         } catch (error) {
-    //             throw new BadKeyError(`failed to parse encrypted private key: ${error.message}`);
-    //         }
-
-    //         const decrypted = await encrypted.decrypt(passphrase);
-
-    //         if (decrypted.algId.algIdent !== "1.3.101.112") {
-    //             throw new BadKeyError(`unknown private key algorithm ${decrypted.algId}`);
-    //         }
-
-    //         const keyData = decodeDer(decrypted.privateKey);
-
-    //         if ("bytes" in keyData) {
-    //             Ed25519PrivateKey.fromBytes(keyData.bytes);
-    //             key = keyData.bytes;
-    //         } else {
-    //             throw new BadKeyError(`expected ASN bytes, got ${JSON.stringify(keyData)}`);
-    //         }
-    //     } else {
-    //         Ed25519PrivateKey.fromBytes(keyStr);
-    //         key = keyStr;
-    //     }
-
-    //     const iv = nacl.randomBytes(16);
-    //     const cipher = crypto.createCipheriv(AES_128_CTR, key.slice(0, 16), iv);
-
-    //     return new EncryptionKey(cipher);
-    // }
 
     public static async fromPassphrase(passphrase: string): Promise<EncryptionKey> {
         const salt = nacl.randomBytes(EncryptionKey.saltLen);
+
         const key = await Pbkdf2.deriveKey(
-            HashAlgorithm.Sha256,
+            HashAlgorithm.Sha384,
             passphrase,
             salt,
             EncryptionKey.c,
             EncryptionKey.dkLen
         );
-        const iv = nacl.randomBytes(16);
 
-        // AES-128-CTR with the first half of the derived key and a random IV
-        const cipher = crypto.createCipheriv(AES_128_CTR, key.slice(0, 16), iv);
-
-        return new EncryptionKey(cipher);
+        return new EncryptionKey(passphrase, key);
     }
 
-    // public async encrypt(text: string): Uint8Array {
-    // }
+    public async encrypt(messageStr: string): Promise<Uint8Array> {
+        // AES-128-CTR with the first half of the derived key and a random IV
+        const message = utf8.encode(messageStr);
+        const iv = nacl.randomBytes(16);
+        const cipher = crypto.createCipheriv(AES_128_CTR, this.key.slice(0, 16), iv);
+
+        const cipherText = Buffer.concat([ cipher.update(message), cipher[ "final" ]() ]);
+
+        const mac = (await Hmac.hash(HashAlgorithm.Sha384, this.key.slice(16), cipherText))
+            .subarray(0, 4);
+
+        const encoded = new Uint8Array(20 + message.length);
+        encoded.set(iv, 0);
+        encoded.set(mac, 16);
+        encoded.set(message, 20);
+        return encoded;
+    }
 }
