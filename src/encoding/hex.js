1ab4513cde0555b615e46807a58f5b41f3c82d62
Ryan Leckey <ryan@launchbadge.com>
2020-05-02 20:56:21 -0700

    chore: setup browser/node unit tests

---
+++ src/encoding/hex.js
@@ -0,0 +1,44 @@
+export function stringify(bytes) {
+    let s = "";
+
+    for (let i = 0; i < bytes.length; i++) {
+        s += encodeQuartet(bytes[i] >>> 4);
+        s += encodeQuartet(bytes[i] & 0x0f);
+    }
+
+    return s;
+}
+
+export function parse(s) {
+    const bytes = new Uint8Array(s.length / 2);
+
+    for (let i = 0; i < s.length; i += 2) {
+        let b0 = decodeQuartet(s.charCodeAt(i));
+        let b1 = decodeQuartet(s.charCodeAt(i + 1));
+
+        bytes[i / 2] = b0 << 4 | b1;
+    }
+
+    return bytes;
+}
+
+function encodeQuartet(b) {
+    return String.fromCharCode(
+        (b + 48) + (((9 - b) >>> 8) & (-48 + 97 - 10)));
+}
+
+function decodeQuartet(c) {
+    let result = 255;
+
+    // 0-9: c > 47 and c < 58
+    result += (((47 - c) & (c - 58)) >> 8) & (-255 + c - 48);
+
+    // A-F: c > 64 and c < 71
+    result += (((64 - c) & (c - 71)) >> 8) & (-255 + c - 65 + 10);
+
+    // a-f: c > 96 and c < 103
+    result += (((96 - c) & (c - 103)) >> 8) & (-255 + c - 97 + 10);
+
+    return result;
+
+}
---
+++ test/unit/encoding/hex.js
@@ -0,0 +1,23 @@
+import * as hex from "../../../src/encoding/hex.js";
+
+describe("hex", () => {
+    const bytes1 = Uint8Array.from([1, 2, 3]);
+    const bytes2 = Uint8Array.from([
+        196, 68, 204, 227, 164, 45, 145, 146, 251, 20, 236, 132, 224, 84,
+        43, 221, 248, 153, 177, 132, 157, 188, 209, 157, 49, 221, 103, 9,
+        63, 139, 112, 77
+    ]);
+
+    const hex1 = "010203";
+    const hex2 = "c444cce3a42d9192fb14ec84e0542bddf899b1849dbcd19d31dd67093f8b704d";
+
+    it("should stringify", () => {
+        expect(hex.stringify(bytes1)).to.equal(hex1);
+        expect(hex.stringify(bytes2)).to.equal(hex2);
+    });
+
+    it("should parse", () => {
+        expect(hex.parse(hex1)).to.deep.equal(bytes1);
+        expect(hex.parse(hex2)).to.deep.equal(bytes2);
+    });
+});
