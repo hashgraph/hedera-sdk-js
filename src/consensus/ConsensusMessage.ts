ffd0f55feedc69c5432b9f1cb08c68e5c114d2de
Kristy Brambila <kristy@launchbadge.com>
2020-07-10 13:22:26 -0700

    feat: add chunk info for ConsensusSubmitMessage

--- src/consensus/ConsensusMessage.ts
+++ src/consensus/ConsensusMessage.ts
@@ -2,6 +2,8 @@ import { ConsensusTopicResponse } from "../generated/MirrorConsensusService_pb";
 import { ConsensusTopicId } from "./ConsensusTopicId";
 import { Time } from "../Time";
 import * as utf8 from "@stablelib/utf8";
+import { ChunkInfo } from "./ConsensusMessageSubmitTransaction";
+import { TransactionId } from "../TransactionId";
 
 export class ConsensusMessage {
     public readonly topicId: ConsensusTopicId;
@@ -9,6 +11,7 @@ export class ConsensusMessage {
     public readonly message: Uint8Array;
     public readonly runningHash: Uint8Array;
     public readonly sequenceNumber: number;
+    public readonly chunkInfo?: ChunkInfo;
 
     public constructor(topicId: ConsensusTopicId, resp: ConsensusTopicResponse) {
         this.topicId = topicId;
@@ -16,6 +19,15 @@ export class ConsensusMessage {
         this.message = resp.getMessage_asU8()!;
         this.runningHash = resp.getRunninghash_asU8();
         this.sequenceNumber = resp.getSequencenumber();
+
+        if (resp.hasChunkinfo()) {
+            const info = resp.getChunkinfo()!;
+            const id = TransactionId._fromProto(info.getInitialtransactionid()!);
+            const total = info.getTotal();
+            const number = info.getNumber();
+
+            this.chunkInfo = { id, total, number };
+        }
     }
 
     public toString(): string {
--- src/consensus/ConsensusMessageSubmitTransaction.ts
+++ src/consensus/ConsensusMessageSubmitTransaction.ts
@@ -2,11 +2,23 @@ import { TransactionBuilder } from "../TransactionBuilder";
 import { Transaction } from "../generated/Transaction_pb";
 import { TransactionResponse } from "../generated/TransactionResponse_pb";
 import { grpc } from "@improbable-eng/grpc-web";
-import { ConsensusSubmitMessageTransactionBody } from "../generated/ConsensusSubmitMessage_pb";
+import { ConsensusSubmitMessageTransactionBody, ConsensusMessageChunkInfo } from "../generated/ConsensusSubmitMessage_pb";
 import { ConsensusService } from "../generated/ConsensusService_pb_service";
 import UnaryMethodDefinition = grpc.UnaryMethodDefinition;
 import { ConsensusTopicId, ConsensusTopicIdLike } from "./ConsensusTopicId";
 import * as utf8 from "@stablelib/utf8";
+import { TransactionId } from "../TransactionId";
+
+export interface ChunkInfo {
+    // TransactionID of the first chunk, gets copied to every subsequent chunk in a fragmented message.
+    id: TransactionId;
+
+    // The total number of chunks in the message.
+    number: number;
+
+    // The sequence number (from 1 to total) of the current chunk in the message.
+    total: number;
+}
 
 export class ConsensusMessageSubmitTransaction extends TransactionBuilder {
     private _body: ConsensusSubmitMessageTransactionBody;
@@ -29,6 +41,17 @@ export class ConsensusMessageSubmitTransaction extends TransactionBuilder {
         } else {
             this._body.setMessage(utf8.encode(message as string));
         }
+        return this;
+    }
+
+    public setChunkInfo(info: ChunkInfo): this {
+        const chunkInfo = new ConsensusMessageChunkInfo();
+        chunkInfo.setInitialtransactionid(info.id._toProto());
+        chunkInfo.setNumber(info.number);
+        chunkInfo.setTotal(info.total);
+
+        this._body.setChunkinfo(chunkInfo);
+
         return this;
     }
 
