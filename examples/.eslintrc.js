d666e05c0124e3899d2c75b5890002a72b168d6c
Kristy Brambila <kristy@launchbadge.com>
2020-07-30 13:42:04 -0700

    chore: upgrade dependencies

--- examples/.eslintrc.js
+++ examples/.eslintrc.js
@@ -1,14 +1,10 @@
 module.exports = {
-  // don't inherit eslint configs from the root eslint since that covers typescript
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: [
-    "@launchbadge/eslint-config",
-  ],
-  rules: {
-    // we use process.env to get the operator private key in examples
-    "no-process-env": "off"
-  }
+    // don't inherit eslint configs from the root eslint since that covers typescript
+    root: true,
+    env: { node: true },
+    extends: [ "@launchbadge/eslint-config" ],
+    rules: {
+        // we use process.env to get the operator private key in examples
+        "no-process-env": "off"
+    }
 };
