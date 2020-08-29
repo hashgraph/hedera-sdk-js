981bdb554c74c48e2a98926bb95484b985a1b012
Ryan Leckey <ryan@launchbadge.com>
2020-08-28 23:13:34 -0700

    fix: suppress @typescript-eslint/explicit-module-boundary-types lint

--- packages/cryptography/.eslintrc.cjs
+++ packages/cryptography/.eslintrc.cjs
@@ -2,4 +2,8 @@ module.exports = {
     parserOptions: {
         tsconfigRootDir: __dirname,
     },
+    rules: {
+        // false positives for jsdoc types
+        "@typescript-eslint/explicit-module-boundary-types": "off"
+    }
 };
