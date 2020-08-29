906b8643a2c6d9f5f87658bcff262d4712a2abc3
Ryan Leckey <ryan@launchbadge.com>
2020-08-28 22:30:20 -0700

    feat(encoding): add base64.encode and improve test vectors for both decode and encode

--- packages/cryptography/src/encoding/base64.js
+++ packages/cryptography/src/encoding/base64.js
@@ -1,13 +1,26 @@
-import { isAccessible } from "../util.js";
 
 /**
  * @param {string} text
  * @returns {Uint8Array}
  */
 export function decode(text) {
-    if (isAccessible("Buffer")) {
+    if (typeof Buffer !== "undefined") {
         return Buffer.from(text, "base64");
-    } else {
-        return Uint8Array.from(window.atob(text), (c) => c.charCodeAt(0));
     }
+
+    // note: assumes <atob> is available in the global scope if <Buffer> is not
+    return Uint8Array.from(atob(text), (c) => c.charCodeAt(0));
+}
+
+/**
+ * @param {Uint8Array} data
+ * @returns {string};
+ */
+export function encode(data) {
+    if (typeof Buffer !== "undefined") {
+        return Buffer.from(data).toString("base64");
+    }
+
+    // note: assumes <btoa> is available in the global scope if <Buffer> is not
+    return btoa(String.fromCharCode.apply(null, Array.from(data)));
 }
--- packages/cryptography/test/unit/encoding/base64.js
+++ packages/cryptography/test/unit/encoding/base64.js
@@ -1,14 +1,27 @@
 import * as base64 from "../../../src/encoding/base64.js";
 import * as utf8 from "../../../src/encoding/utf8.js";
 
-const encoded =
-    "TG9yZW0gaXBzdW0gZG9sb3Igc2l0IGFtZXQsIGNvbnNlY3RldHVyIGFkaXBpc2NpbmcgZWxpdC4gUGhhc2VsbHVzIGNvbnNlY3RldHVyIGV4IGRpYW0sIGV0IGZhdWNpYnVzIGxpYmVybyB1bHRyaWNpZXMgaWQuIENyYXMgY29uc2VjdGV0dXIgdmVsaXQgbGFjdXMsIHZlbCBwdWx2aW5hciBtYXVyaXMgZGlnbmlzc2ltIHNpdCBhbWV0LiBOdWxsYSB1dCBtb2xlc3RpZSBtYXNzYSwgcXVpcyB2dWxwdXRhdGUgZXJhdC4gVml2YW11cyBhIG1hdXJpcyBxdWFtLiBOdWxsYSB2dWxwdXRhdGUgY29uc2VjdGV0dXIgY29udmFsbGlzLiBDdXJhYml0dXIgdXQgdGVtcG9yIGxhY3VzLiBNb3JiaSBhdWN0b3IgdmVsaXQgZXQgbmliaCBmYXVjaWJ1cyBtYXhpbXVzLiBWZXN0aWJ1bHVtIGV1IHRvcnRvciB1bHRyaWNpZXMsIGNvbmRpbWVudHVtIGZlbGlzIHZlbCwgc3VzY2lwaXQgbGVvLiBWZXN0aWJ1bHVtIGxhb3JlZXQsIG9yY2kgdXQgc3VzY2lwaXQgc29sbGljaXR1ZGluLCBuaXNpIHNhcGllbiBhbGlxdWV0IGVuaW0sIGV1IHZhcml1cyByaXN1cyBtYWduYSB2aXRhZSBuaXNsLiBEb25lYyBtYXVyaXMgbGFjdXMsIHBoYXJldHJhIG5lYyB2YXJpdXMgZWdldCwgc2NlbGVyaXNxdWUgc2NlbGVyaXNxdWUgcHVydXMuIFBlbGxlbnRlc3F1ZSBwb3J0YSB2b2x1dHBhdCBlbmltIHZpdGFlIGJsYW5kaXQu";
+// from RFC 4648
+const vectors = [
+    ["", ""],
+    ["f", "Zg=="],
+    ["fo", "Zm8="],
+    ["foo", "Zm9v"],
+    ["foob", "Zm9vYg=="],
+    ["fooba", "Zm9vYmE="],
+    ["foobar", "Zm9vYmFy"]
+];
 
-const decoded =
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus consectetur ex diam, et faucibus libero ultricies id. Cras consectetur velit lacus, vel pulvinar mauris dignissim sit amet. Nulla ut molestie massa, quis vulputate erat. Vivamus a mauris quam. Nulla vulputate consectetur convallis. Curabitur ut tempor lacus. Morbi auctor velit et nibh faucibus maximus. Vestibulum eu tortor ultricies, condimentum felis vel, suscipit leo. Vestibulum laoreet, orci ut suscipit sollicitudin, nisi sapien aliquet enim, eu varius risus magna vitae nisl. Donec mauris lacus, pharetra nec varius eget, scelerisque scelerisque purus. Pellentesque porta volutpat enim vitae blandit.";
+describe("encoding/base64", function () {
+    it("should encode", function () {
+        for (const [decoded, encoded] of vectors) {
+            expect(base64.encode(utf8.encode(decoded))).to.equal(encoded);
+        }
+    });
 
-describe("hedera/base64", function () {
-    it("should decode correctly", function () {
-        expect(utf8.decode(base64.decode(encoded))).to.deep.equal(decoded);
+    it("should decode", function () {
+        for (const [decoded, encoded] of vectors) {
+            expect(utf8.decode(base64.decode(encoded))).to.equal(decoded);
+        }
     });
 });
