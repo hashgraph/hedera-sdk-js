fc21ee44e4dec7f8fd46765049aff25737d8b58a
Ryan Leckey <ryan@launchbadge.com>
2020-09-03 00:38:12 -0700

    fix: add setContractId to AccountBalanceQuery

--- src/account/AccountBalanceQuery.js
+++ src/account/AccountBalanceQuery.js
@@ -1,15 +1,24 @@
 import Query from "../Query";
 import AccountId from "./AccountId";
+import ContractId from "../contract/ContractId";
 import proto from "@hashgraph/proto";
 import Hbar from "../Hbar";
 
 /**
+ * Get the balance of a Hedera™ crypto-currency account.
+ *
+ * This returns only the balance, so its a smaller and faster reply
+ * than AccountInfoQuery.
+ *
+ * This query is free.
+ *
  * @augments {Query<Hbar>}
  */
 export default class AccountBalanceQuery extends Query {
     /**
      * @param {object} properties
      * @param {(AccountId | string)=} properties.accountId
+     * @param {(ContractId | string)=} properties.contractId
      */
     constructor(properties) {
         super();
@@ -20,22 +29,50 @@ export default class AccountBalanceQuery extends Query {
          */
         this._accountId = null;
 
+        /**
+         * @type {?ContractId}
+         * @private
+         */
+
         if (properties?.accountId != null) {
             this.setAccountId(properties?.accountId);
+        }
+
+        if (properties?.contractId != null) {
+            this.setContractId(properties?.contractId);
         }
     }
 
     /**
      * Set the account ID for which the balance is being requested.
      *
+     * This is mutually exclusive with `setContractId`.
+     *
      * @param {AccountId | string} accountId
-     * @returns {AccountBalanceQuery}
+     * @returns {this}
      */
     setAccountId(accountId) {
         this._accountId =
             accountId instanceof AccountId
                 ? accountId
                 : AccountId.fromString(accountId);
+
+        return this;
+    }
+
+    /**
+     * Set the contract ID for which the balance is being requested.
+     *
+     * This is mutually exclusive with `setAccountId`.
+     *
+     * @param {ContractId | string} contractId
+     * @returns {this}
+     */
+    setContractId(contractId) {
+        this._contractId =
+            contractId instanceof ContractId
+                ? contractId
+                : ContractId.fromString(contractId);
 
         return this;
     }
@@ -63,7 +100,7 @@ export default class AccountBalanceQuery extends Query {
             cryptogetAccountBalance: {
                 header: queryHeader,
                 accountID: this._accountId?._toProtobuf(),
-                contractID: null,
+                contractID: this._contractId?._toProtobuf(),
             },
         };
     }
