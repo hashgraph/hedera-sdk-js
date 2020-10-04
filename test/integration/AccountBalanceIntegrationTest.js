29562329f3082c80eb309fecfc07c48fc0e1f601
Ryan Leckey <ryan@launchbadge.com>
2020-10-04 01:36:01 -0700

    test: simply AccountBalanceIntegrationTest to remove dependencies on success (now only requires free queries)

--- test/integration/AccountBalanceIntegrationTest.js
+++ test/integration/AccountBalanceIntegrationTest.js
@@ -1,49 +1,32 @@
-import AccountCreateTransaction from "../src/account/AccountCreateTransaction";
-import AccountDeleteTransaction from "../src/account/AccountDeleteTransaction";
-import AccountBalanceQuery from "../src/account/AccountBalanceQuery";
 import Hbar from "../src/Hbar";
-import TransactionId from "../src/TransactionId";
-import newClient from "./IntegrationClient";
-import { PrivateKey } from "../src/index";
+import AccountBalanceQuery from "../src/account/AccountBalanceQuery";
+import { newIntegrationClient } from "./client";
 
-describe("AccountBalance", function () {
-    it("should be executable", async function () {
-        this.timeout(10000);
+describe("AccountBalanceQuery", function () {
+    this.timeout(10000);
 
-        const client = newClient();
-        const operatorId = client.getOperatorId();
-        const key = PrivateKey.generate();
+    let client;
 
-        const response = await new AccountCreateTransaction()
-            .setKey(key.publicKey)
-            .setMaxTransactionFee(new Hbar(2))
-            .setInitialBalance(new Hbar(1))
-            .execute(client);
+    before(function () {
+        client = newIntegrationClient();
+    });
 
-        const receipt = await response.getReceipt(client);
+    describe("#execute", function () {
+        it("account 0.0.3 should have a balance higher than 1 hbar", async function () {
+            const balance = await new AccountBalanceQuery()
+                .setAccountId("3") // balance of node 3
+                .execute(client);
 
-        expect(receipt.accountId).to.not.be.null;
-        const account = receipt.accountId;
+            expect(balance.toTinybars().toNumber()).to.be.greaterThan(
+                new Hbar(1).toTinybars().toNumber()
+            );
+        });
 
-        const balance = await new AccountBalanceQuery()
-            .setAccountId(account)
-            .setNodeId(response.nodeId)
-            .execute(client);
-
-        expect(balance.toTinybars().toInt()).to.be.equal(
-            new Hbar(1).toTinybars().toInt()
-        );
-
-        await (
-            await (
-                await new AccountDeleteTransaction()
-                    .setAccountId(account)
-                    .setNodeId(response.nodeId)
-                    .setTransferAccountId(operatorId)
-                    .setTransactionId(TransactionId.generate(account))
-                    .freezeWith(client)
-                    .sign(key)
-            ).execute(client)
-        ).getReceipt(client);
+        // TODO: After we have PreCheckError
+        // it("an account that does not exist should return an error", async function () {
+        //     await new AccountBalanceQuery()
+        //         .setAccountId("0.0.21849819482")
+        //         .execute(client);
+        // });
     });
 });
