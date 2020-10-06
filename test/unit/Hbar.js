d8638501f3af00fc9cecb408d963822ad8653564
Ryan Leckey <ryan@launchbadge.com>
2020-10-06 00:23:19 -0700

    add a test for hbar to tinybar conversion

--- test/unit/Hbar.js
+++ test/unit/Hbar.js
@@ -4,4 +4,11 @@ describe("Hbar", function () {
     it("should stringify to hbars", function () {
         expect(new Hbar(10).toString()).to.eql("10 ℏ");
     });
+
+    it("should convert from hbars to tinybars", function () {
+        const tinybar = "98327923153";
+        const hbar = new Hbar("983.27923153");
+
+        expect(tinybar).to.eql(hbar.toTinybars().toString());
+    });
 });
