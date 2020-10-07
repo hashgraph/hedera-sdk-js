27f9759b9b8e96e8d78d5145902ef93026496e81
Daniel Akhterov <akhterovd@gmail.com>
2020-10-07 16:53:23 -0700

    refactor: update `arrayStartsWith` to use direct indexing instead of `DataView.getUint8`

--- packages/cryptography/src/util/array.js
+++ packages/cryptography/src/util/array.js
@@ -32,25 +32,13 @@ export function arrayEqual(array1, array2) {
  * @returns {boolean}
  */
 export function arrayStartsWith(array, arrayPrefix) {
-    // console.log("Lengths", array.byteLength, arrayPrefix.byteLength);
     if (array.byteLength < arrayPrefix.byteLength) {
         return false;
     }
 
-    // for (let j = 0; j < arrayPrefix.length; j++) {
-    //     console.log("Values", array[j], arrayPrefix[j]);
-    // }
-
-    // console.log("-----------------------------------------");
-
-    // const view1 = new DataView(array.buffer, 0, arrayPrefix.byteLength);
-    // const view2 = new DataView(arrayPrefix.buffer);
-
     let i = arrayPrefix.byteLength;
 
     while (i--) {
-        // console.log("Values", view1.getUint8(i), view2.getUint8(i));
-        // if (view1.getUint8(i) !== view2.getUint8(i)) {
         if (array[i] !== arrayPrefix[i]) {
             return false;
         }
