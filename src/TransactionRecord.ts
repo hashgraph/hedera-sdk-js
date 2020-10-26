f424aa167cc0e6b7c4050f1eb05fc3aafff879f0
Daniel Akhterov <akhterovd@gmail.com>
2020-10-26 14:37:14 -0700

    feat: add support for `TransactionRecord.tokenTransfers`

--- src/TransactionRecord.ts
+++ src/TransactionRecord.ts
@@ -3,13 +3,100 @@ import { ContractFunctionResult } from "./contract/ContractFunctionResult";
 import { TransactionRecord as ProtoTransactionRecord } from "./generated/TransactionRecord_pb";
 import { TransactionId } from "./TransactionId";
 import { TransferList as ProtoTransferList } from "./generated/CryptoTransfer_pb";
+import { TokenTransferList as ProtoTokenTransferList, AccountAmount as ProtoAccountAmount } from "./generated/BasicTypes_pb";
 import { Time } from "./Time";
 import { Hbar } from "./Hbar";
+import { TokenId, TokenIdLike } from "./token/TokenId";
+import { AccountId, AccountIdLike } from "./account/AccountId";
 import { Transfer, transferFromProto } from "./Transfer";
+import BigNumber from "bignumber.js";
 
 const callResult = Symbol("callResult");
 
 const callResultIsCreate = Symbol("callResultIsCreate");
+
+export class AccountTokenTransfersMap {
+    private _transfers: Map<string, BigNumber[]> = new Map();
+
+    public constructor(transfers: ProtoAccountAmount[]) {
+        for (const transfer of transfers) {
+            const accountId = AccountId._fromProto(transfer.getAccountid()!).toString();
+            let list = this._transfers.get(accountId);
+
+            if (list == null) {
+                list = [];
+                this._transfers.set(accountId, list);
+            }
+
+            list.push(new BigNumber(transfer.getAmount()!));
+        }
+    }
+
+    public get(accountId: AccountIdLike): BigNumber[] | undefined {
+        const account = new AccountId(accountId).toString();
+        return this._transfers.get(account);
+    }
+
+    public values(): IterableIterator<BigNumber[]> {
+        return this._transfers.values();
+    }
+
+    public keys(): IterableIterator<AccountId> {
+        const keys = [];
+        for (const key of this._transfers.keys()) {
+            keys.push(AccountId.fromString(key));
+        }
+        return keys[ Symbol.iterator ]();
+    }
+
+    public [ Symbol.iterator ](): IterableIterator<[AccountId, BigNumber[]]> {
+        const map = new Map();
+        for (const [ key, value ] of this._transfers) {
+            map.set(AccountId.fromString(key), value);
+        }
+        return map[ Symbol.iterator ]();
+    }
+}
+
+export class TokenTransfersMap {
+    private _transfers: Map<string, AccountTokenTransfersMap> = new Map();
+
+    public constructor(transfers: ProtoTokenTransferList[]) {
+        for (const list of transfers) {
+            const tokenId = TokenId._fromProto(list.getToken()!);
+
+            this._transfers.set(
+                tokenId.toString(),
+                new AccountTokenTransfersMap(list.getTransfersList())
+            );
+        }
+    }
+
+    public get(tokenId: TokenIdLike): AccountTokenTransfersMap | undefined {
+        const token = new TokenId(tokenId).toString();
+        return this._transfers.get(token);
+    }
+
+    public values(): IterableIterator<AccountTokenTransfersMap> {
+        return this._transfers.values();
+    }
+
+    public keys(): IterableIterator<TokenId> {
+        const keys = [];
+        for (const key of this._transfers.keys()) {
+            keys.push(TokenId.fromString(key));
+        }
+        return keys[ Symbol.iterator ]();
+    }
+
+    public [ Symbol.iterator ](): IterableIterator<[TokenId, AccountTokenTransfersMap]> {
+        const map = new Map();
+        for (const [ key, value ] of this._transfers) {
+            map.set(TokenId.fromString(key), value);
+        }
+        return map[ Symbol.iterator ]();
+    }
+}
 
 /**
  * Response when the client sends the node TransactionGetRecordResponse.
@@ -59,6 +146,8 @@ export class TransactionRecord {
      */
     public readonly transfers: Transfer[];
 
+    public readonly tokenTransfers: TokenTransfersMap;
+
     private constructor(record: ProtoTransactionRecord) {
         this.receipt = TransactionReceipt._fromProto(record.getReceipt()!);
         this.transactionHash = record.getTransactionhash_asU8();
@@ -74,6 +163,8 @@ export class TransactionRecord {
         } else if (record.hasContractcreateresult()) {
             this[ callResult ] = new ContractFunctionResult(record.getContractcreateresult()!);
         }
+
+        this.tokenTransfers = new TokenTransfersMap(record.getTokentransferlistsList());
     }
 
     public static _fromProto(pb: ProtoTransactionRecord): TransactionRecord {
