522eb844ac958e29905ddad3ae300f060633d34c
Daniel Akhterov <akhterovd@gmail.com>
2020-11-02 13:57:05 -0800

    fix: remove commented console logs

--- build/replace-module-paths.cjs
+++ build/replace-module-paths.cjs
@@ -1,11 +1,6 @@
 // ensure that all local imports go from .js to .cjs when moved to lib/
 // https://github.com/liady/babel-plugin-module-rewrite
 module.exports = function replaceImport(originalPath, callingFileName, options) {
-
-    // console.log("----------------------------------------------------------------------------------------------------");
-    // console.log(originalPath);
-    // console.log(callingFileName);
-
     let path = originalPath;
 
     if (
@@ -17,9 +12,6 @@ module.exports = function replaceImport(originalPath, callingFileName, options) 
         path = path.replace(".js", ".cjs");
         path = path.replace("src/", "lib/");
     }
-
-    // console.log(path);
-    // console.log("----------------------------------------------------------------------------------------------------");
 
     return path;
 }
