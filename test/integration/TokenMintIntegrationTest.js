26bc2d28ced44d8a0e303c68436dc687451d8f22
Ricky Saechao <ricky@launchbadge.com>
2021-01-05 21:45:42 +0000

    Feature/burn mint integration

---
+++ test/integration/TokenMintIntegrationTest.js
@@ -0,0 +1,237 @@
+import {
+    AccountCreateTransaction,
+    TokenCreateTransaction,
+    TokenDeleteTransaction,
+    TokenMintTransaction,
+    AccountDeleteTransaction,
+    TransactionId,
+    Hbar,
+    Status,
+    PrivateKey,
+} from "../src/exports.js";
+import newClient from "./client/index.js";
+
+describe("TokenMint", function () {
+    it("should be executable", async function () {
+        this.timeout(20000);
+
+        const client = await newClient();
+        const operatorId = client.operatorAccountId;
+        const operatorKey = client.operatorPublicKey;
+        const key = PrivateKey.generate();
+
+        const response = await new AccountCreateTransaction()
+            .setKey(key.publicKey)
+            .setMaxTransactionFee(new Hbar(2))
+            .setInitialBalance(new Hbar(1))
+            .execute(client);
+
+        const receipt = await response.getReceipt(client);
+
+        expect(receipt.accountId).to.not.be.null;
+        const account = receipt.accountId;
+
+        const token = await new TokenCreateTransaction()
+            .setTokenName("ffff")
+            .setTokenSymbol("F")
+            .setDecimals(3)
+            .setInitialSupply(1000000)
+            .setTreasuryAccountId(operatorId)
+            .setAdminKey(operatorKey)
+            .setKycKey(operatorKey)
+            .setFreezeKey(operatorKey)
+            .setWipeKey(operatorKey)
+            .setSupplyKey(operatorKey)
+            .setFreezeDefault(false)
+            .setMaxTransactionFee(new Hbar(1000))
+            .execute(client);
+
+        const tokenId = (await token.getReceipt(client)).tokenId;
+
+        await (
+            await new TokenMintTransaction()
+                .setAmount(10)
+                .setTokenId(tokenId)
+                .execute(client)
+        ).getReceipt(client);
+
+        await (
+            await new TokenDeleteTransaction()
+                .setTokenId(tokenId)
+                .execute(client)
+        ).getReceipt(client);
+
+        await (
+            await (
+                await new AccountDeleteTransaction()
+                    .setAccountId(account)
+                    .setMaxTransactionFee(new Hbar(1))
+                    .setNodeAccountIds([response.nodeId])
+                    .setTransferAccountId(operatorId)
+                    .setTransactionId(TransactionId.generate(account))
+                    .freezeWith(client)
+                    .sign(key)
+            ).execute(client)
+        ).getReceipt(client);
+    });
+
+    it("should error when token ID is not set", async function () {
+        this.timeout(10000);
+
+        const client = await newClient();
+
+        let err = false;
+
+        try {
+            await (
+                await new TokenMintTransaction().setAmount(10).execute(client)
+            ).getReceipt(client);
+        } catch (error) {
+            err = error.toString().includes(Status.InvalidTokenId);
+        }
+
+        if (!err) {
+            throw new Error("token Mint did not error");
+        }
+    });
+
+    it("should error when amount is not set", async function () {
+        this.timeout(20000);
+
+        const client = await newClient();
+        const operatorId = client.operatorAccountId;
+        const operatorKey = client.operatorPublicKey;
+        const key = PrivateKey.generate();
+
+        const response = await new AccountCreateTransaction()
+            .setKey(key)
+            .setInitialBalance(new Hbar(1))
+            .execute(client);
+
+        const account = (await response.getReceipt(client)).accountId;
+
+        const token = (
+            await (
+                await new TokenCreateTransaction()
+                    .setTokenName("ffff")
+                    .setTokenSymbol("F")
+                    .setDecimals(3)
+                    .setInitialSupply(1000000)
+                    .setTreasuryAccountId(operatorId)
+                    .setAdminKey(operatorKey)
+                    .setKycKey(operatorKey)
+                    .setFreezeKey(operatorKey)
+                    .setWipeKey(operatorKey)
+                    .setSupplyKey(operatorKey)
+                    .setFreezeDefault(false)
+                    .setMaxTransactionFee(new Hbar(1000))
+                    .execute(client)
+            ).getReceipt(client)
+        ).tokenId;
+
+        let err = false;
+
+        try {
+            await (
+                await new TokenMintTransaction()
+                    .setTokenId(token)
+                    .execute(client)
+            ).getReceipt(client);
+        } catch (error) {
+            err = error.toString().includes(Status.InvalidTokenMintAmount);
+        }
+
+        await (
+            await (
+                await new AccountDeleteTransaction()
+                    .setAccountId(account)
+                    .setMaxTransactionFee(new Hbar(1))
+                    .setNodeAccountIds([response.nodeId])
+                    .setTransferAccountId(operatorId)
+                    .setTransactionId(TransactionId.generate(account))
+                    .freezeWith(client)
+                    .sign(key)
+            ).execute(client)
+        ).getReceipt(client);
+
+        await (
+            await new TokenDeleteTransaction().setTokenId(token).execute(client)
+        ).getReceipt(client);
+
+        if (!err) {
+            throw new Error("token burn did not error");
+        }
+    });
+
+    it("should error when admin key does not sign transaction", async function () {
+        this.timeout(10000);
+
+        const client = await newClient();
+        const operatorId = client.operatorAccountId;
+        const operatorKey = client.operatorPublicKey;
+        const key = PrivateKey.generate();
+
+        const response = await new AccountCreateTransaction()
+            .setKey(key.publicKey)
+            .setMaxTransactionFee(new Hbar(2))
+            .setInitialBalance(new Hbar(1))
+            .execute(client);
+
+        const receipt = await response.getReceipt(client);
+
+        expect(receipt.accountId).to.not.be.null;
+        const account = receipt.accountId;
+
+        const token = await new TokenCreateTransaction()
+            .setTokenName("ffff")
+            .setTokenSymbol("F")
+            .setDecimals(3)
+            .setInitialSupply(1000000)
+            .setTreasuryAccountId(operatorId)
+            .setAdminKey(operatorKey)
+            .setKycKey(operatorKey)
+            .setFreezeKey(operatorKey)
+            .setWipeKey(operatorKey)
+            .setSupplyKey(operatorKey)
+            .setFreezeDefault(false)
+            .setMaxTransactionFee(new Hbar(1000))
+            .execute(client);
+
+        const tokenId = (await token.getReceipt(client)).tokenId;
+
+        let err = false;
+
+        await (
+            await new TokenMintTransaction()
+                .setAmount(10)
+                .setTokenId(tokenId)
+                .execute(client)
+        ).getReceipt(client);
+
+        await (
+            await new TokenDeleteTransaction()
+                .setTokenId(tokenId)
+                .execute(client)
+        ).getReceipt(client);
+
+        try {
+            await (
+                await (
+                    await new AccountDeleteTransaction()
+                        .setAccountId(account)
+                        .setMaxTransactionFee(new Hbar(1))
+                        .setNodeAccountIds([response.nodeId])
+                        .setTransferAccountId(operatorId)
+                        .setTransactionId(TransactionId.generate(account))
+                        .freezeWith(client)
+                ).execute(client)
+            ).getReceipt(client);
+        } catch (error) {
+            err = error.toString().includes(Status.InvalidSignature);
+        }
+
+        if (!err) {
+            throw new Error("token Mint did not error");
+        }
+    });
+});
