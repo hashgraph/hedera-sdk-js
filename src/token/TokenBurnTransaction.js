6996276b661aac3957175d3bf2d6360135cb77c5
Daniel Akhterov <akhterovd@gmail.com>
2021-02-19 13:33:23 -0800

    fix: `burnToken` -> `tokenBurn`

--- src/token/TokenBurnTransaction.js
+++ src/token/TokenBurnTransaction.js
@@ -71,7 +71,7 @@ export default class TokenBurnTransaction extends Transaction {
         bodies
     ) {
         const body = bodies[0];
-        const burnToken = /** @type {proto.ITokenBurnTransactionBody} */ (body.burnToken);
+        const burnToken = /** @type {proto.ITokenBurnTransactionBody} */ (body.tokenBurn);
 
         return Transaction._fromProtobufTransactions(
             new TokenBurnTransaction({
