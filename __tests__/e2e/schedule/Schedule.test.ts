dce92a36279ff41c6ddfc7e9c2f68c1e4b09c9a4
Andrei Kuzmiankov <andrei@launchbadge.com>
2021-04-30 19:44:10 -0700

    feat: Added schedule signing test.

--- __tests__/e2e/schedule/Schedule.test.ts
+++ __tests__/e2e/schedule/Schedule.test.ts
@@ -4,11 +4,15 @@ import {
     ScheduleInfoQuery,
     ScheduleDeleteTransaction,
     ScheduleSignTransaction,
+    ConsensusMessageSubmitTransaction,
+    ConsensusTopicCreateTransaction,
     Ed25519PrivateKey,
-    Hbar
+    KeyList,
+    Hbar, TransferTransaction
 } from "../../../src/index-node";
 import * as utf8 from "@stablelib/utf8";
 import { getClientForIntegrationTest } from "../client-setup";
+// import { ConsensusTopicCreateTransaction } from "../../../lib/consensus/ConsensusTopicCreateTransaction";
 
 describe("ScheduleCreateTransaction", () => {
     it("can be executed", async() => {
@@ -74,5 +78,47 @@ describe("ScheduleCreateTransaction", () => {
             .execute(client);
         console.log(info.getTransaction().toString());
         console.log(info.executionTime?.toDateString());
+    }, 60000);
+
+    it("schedule transaction can be signed", async() => {
+        const client = await getClientForIntegrationTest();
+
+        const key1 = await Ed25519PrivateKey.generate();
+        // Submit Key
+        const key2 = await Ed25519PrivateKey.generate();
+
+        let transactionId = await new AccountCreateTransaction()
+            .setInitialBalance(new Hbar(10))
+            .setKey(key1.publicKey)
+            .execute(client);
+
+        let receipt = await transactionId.getReceipt(client);
+        const accountId = receipt.getAccountId();
+
+        const transaction = await new TransferTransaction()
+            .addHbarTransfer(accountId, new Hbar(1)
+                .asTinybar()
+                .dividedBy(10)
+                .negated())
+            .addHbarTransfer(client._getOperatorAccountId()!, new Hbar(1).asTinybar().dividedBy(10))
+            .setMaxTransactionFee(new Hbar(1).asTinybar().dividedBy(2))
+            .build(client);
+
+        const scheduled = transaction
+            .schedule()
+            .setPayerAccountId(accountId)
+            .build(client);
+        transactionId = await scheduled.execute(client);
+        receipt = await transactionId.getReceipt(client);
+        const scheduleId = receipt.getScheduleId();
+
+        await (
+            await (
+                await new ScheduleSignTransaction()
+                    .setScheduleId(scheduleId)
+                    .build(client)
+                    .sign(key1)
+            ).execute(client)
+        ).getReceipt(client);
     }, 60000);
 });
