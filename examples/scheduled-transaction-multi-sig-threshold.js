91bb6b0d0abf8de4ec74d2662c079fafac3cec1b
derdis14 <78613993+derdis14@users.noreply.github.com>
2021-05-08 23:44:45 +0200

    added example: scheduled transaction multi-sig with threshold
    Signed-off-by: derdis14 <78613993+derdis14@users.noreply.github.com>

---
+++ examples/scheduled-transaction-multi-sig-threshold.js
@@ -0,0 +1,124 @@
+require("dotenv").config();
+const {
+    Client,
+    PrivateKey,
+    AccountId,
+    KeyList,
+    AccountCreateTransaction,
+    Hbar,
+    AccountBalanceQuery,
+    TransferTransaction,
+    ScheduleSignTransaction,
+    ScheduleInfoQuery,
+    TransactionRecordQuery,
+} = require("@hashgraph/sdk");
+
+async function main() {
+    // set up client
+    let client;
+    if (process.env.HEDERA_NETWORK != null) {
+        switch (process.env.HEDERA_NETWORK) {
+            case "previewnet":
+                client = Client.forPreviewnet();
+                break;
+            default:
+                client = Client.forTestnet();
+        }
+    } else {
+        try {
+            client = await Client.fromConfigFile(process.env.CONFIG_FILE);
+        } catch (err) {
+            client = Client.forTestnet();
+        }
+    }
+    if (process.env.OPERATOR_KEY != null && process.env.OPERATOR_ID != null) {
+        const operatorKey = PrivateKey.fromString(process.env.OPERATOR_KEY);
+        const operatorId = AccountId.fromString(process.env.OPERATOR_ID);
+        client.setOperator(operatorId, operatorKey);
+    }
+
+    // generate keys
+    const privateKeyList = [];
+    const publicKeyList = [];
+    for (let i = 0; i < 4; i++) {
+        const privateKey = PrivateKey.generate();
+        const publicKey = privateKey.publicKey;
+        privateKeyList.push(privateKey);
+        publicKeyList.push(publicKey);
+        console.log(`${i + 1}. public key: ${publicKey}`);
+        console.log(`${i + 1}. private key: ${privateKey}`);
+    }
+    const thresholdKey = new KeyList(publicKeyList, 3);
+
+    // create multi-sig account
+    const txAccountCreate = await new AccountCreateTransaction()
+        .setKey(thresholdKey)
+        .setInitialBalance(Hbar.fromTinybars(1))
+        .setAccountMemo("3-of-4 multi-sig account")
+        .execute(client);
+
+    const txAccountCreateReceipt = await txAccountCreate.getReceipt(client);
+    const multiSigAccountId = txAccountCreateReceipt.accountId;
+    console.log("3-of-4 multi-sig account ID: " + multiSigAccountId);
+    await queryBalance(multiSigAccountId, client);
+
+    // schedule crypto transfer from multi-sig account to operator account
+    const txSchedule = await (await new TransferTransaction()
+        .addHbarTransfer(multiSigAccountId, Hbar.fromTinybars(-1))
+        .addHbarTransfer(client.operatorAccountId, Hbar.fromTinybars(1))
+        .schedule() // create schedule
+        .freezeWith(client)
+        .sign(privateKeyList[0])) // add 1. signature
+        .execute(client);
+
+    const txScheduleReceipt = await txSchedule.getReceipt(client);
+    console.log("Schedule status: " + txScheduleReceipt.status.toString());
+    const scheduleId = txScheduleReceipt.scheduleId;
+    console.log("Schedule ID: " + scheduleId);
+    const scheduledTxId = txScheduleReceipt.scheduledTransactionId;
+    console.log("Scheduled tx ID: " + scheduledTxId);
+
+    // add 2. signature
+    const txScheduleSign1 = await (await new ScheduleSignTransaction()
+        .setScheduleId(scheduleId)
+        .freezeWith(client)
+        .sign(privateKeyList[1]))
+        .execute(client);
+
+    const txScheduleSign1Receipt = await txScheduleSign1.getReceipt(client);
+    console.log("1. ScheduleSignTransaction status: " + txScheduleSign1Receipt.status.toString());
+    await queryBalance(multiSigAccountId, client);
+
+    // add 3. signature to trigger scheduled tx
+    const txScheduleSign2 = await (await new ScheduleSignTransaction()
+        .setScheduleId(scheduleId)
+        .freezeWith(client)
+        .sign(privateKeyList[2]))
+        .execute(client);
+
+    const txScheduleSign2Receipt = await txScheduleSign2.getReceipt(client);
+    console.log("2. ScheduleSignTransaction status: " + txScheduleSign2Receipt.status.toString());
+    await queryBalance(multiSigAccountId, client);
+
+    // query schedule
+    const scheduleInfo = await new ScheduleInfoQuery()
+        .setScheduleId(scheduleId)
+        .execute(client);
+    console.log(scheduleInfo);
+
+    // query triggered scheduled tx
+    const recordScheduledTx = await new TransactionRecordQuery()
+        .setTransactionId(scheduledTxId)
+        .execute(client);
+    console.log(recordScheduledTx);
+}
+
+async function queryBalance(accoundId, client) {
+    const accountBalance = await new AccountBalanceQuery()
+        .setAccountId(accoundId)
+        .execute(client);
+    console.log("Balance of account " + accoundId + ": " + accountBalance.hbars.toTinybars() + " tinybar");
+    return accountBalance;
+}
+
+main();
