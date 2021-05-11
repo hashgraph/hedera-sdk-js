7ef3b67445ff89fc286c2b2263fec6eccc352da7
Daniel Akhterov <akhterovd@gmail.com>
2021-05-11 15:43:00 -0700

    chore: rebase onto master and update browser to use `IntegrationTestEnv`
    Signed-off-by: Daniel Akhterov <akhterovd@gmail.com>

--- test/integration/client/browser.js
+++ test/integration/client/browser.js
@@ -5,55 +5,69 @@ import {
     Hbar,
 } from "../../src/exports.js";
 
-/**
- * @param {boolean} createNewAccount
- */
-export async function newIntegrationClient(createNewAccount = false) {
-    let client;
+export default class IntegrationTestEnv {
+    /**
+     * @type {Client} client
+     * @type {PrivateKey} operatorKey
+     * @type {AccountId} operatorId
+     * @type {AccountId[]} nodeAccountIds
+     */
+    constructor(client, operatorKey, operatorId, nodeAccountIds) {
+        /** @type {Client} */
+        this.client = client;
 
-    if (
-        import.meta.env.VITE_HEDERA_NETWORK != null &&
-        import.meta.env.VITE_HEDERA_NETWORK == "previewnet"
-    ) {
-        client = Client.forPreviewnet();
-    } else {
-        client = Client.forTestnet();
+        /** @type {PrivateKey} */
+        this.operatorKey = operatorKey;
+
+        /** @type {AccountId} */
+        this.operatorId = operatorId;
+
+        /** @type {[]} */
+        this.nodeAccountIds = nodeAccountIds;
+
+        Object.freeze(this);
     }
 
-    try {
-        const operatorId =
-            import.meta.env.VITE_OPERATOR_ID ||
-            import.meta.env.VITE_OPERATOR_ID;
+    static async new() {
+        let client;
 
-        const operatorKey =
-            import.meta.env.VITE_OPERATOR_KEY ||
-            import.meta.env.VITE_OPERATOR_KEY;
+        if (
+            import.meta.env.VITE_HEDERA_NETWORK != null &&
+            import.meta.env.VITE_HEDERA_NETWORK == "previewnet"
+        ) {
+            client = Client.forPreviewnet();
+        } else {
+            client = Client.forTestnet();
+        }
 
-        client.setOperator(operatorId, operatorKey);
-    } catch (err) {
-        // ignore error and complain later
-    }
+        try {
+            const operatorId =
+                import.meta.env.VITE_OPERATOR_ID ||
+                import.meta.env.VITE_OPERATOR_ID;
 
-    expect(client.operatorAccountId).to.not.be.null;
-    expect(client.operatorPublicKey).to.not.be.null;
+            const operatorKey =
+                import.meta.env.VITE_OPERATOR_KEY ||
+                import.meta.env.VITE_OPERATOR_KEY;
 
-    if (createNewAccount) {
+            client.setOperator(operatorId, operatorKey);
+        } catch (err) {
+            // ignore error and complain later
+        }
+
+        expect(client.operatorAccountId).to.not.be.null;
+        expect(client.operatorPublicKey).to.not.be.null;
+
         var key = PrivateKey.generate();
 
-        var accountId = (
-            await (
-                await new AccountCreateTransaction()
-                    .setKey(key)
-                    .setInitialBalance(new Hbar(100))
-                    .execute(client)
-            ).getReceipt(client)
-        ).accountId;
+        const response = await new AccountCreateTransaction()
+            .setKey(key)
+            .setInitialBalance(new Hbar(100))
+            .execute(client);
+
+        var accountId = (await response.getReceipt(client)).accountId;
 
         client.setOperator(accountId, key);
+
+        return new IntegrationTestEnv(client, key, accountId, [response.nodeId]);
     }
-
-    return client;
 }
-
-// TODO: Remove this
-export default newIntegrationClient;
