4558466d83f0e1ad4153019e16246e3aee1d9fd5
Ryan Leckey <ryan@launchbadge.com>
2021-07-01 14:34:43 -0700

    fix(proto): ensure that all types are exported both as types _and_ as values

--- packages/proto/src/index.js
+++ packages/proto/src/index.js
@@ -1,9 +1,17 @@
 import * as $protobuf from "protobufjs/minimal.js";
 import { proto } from "./proto.js";
 
+// ============================================================================
+// Utility
+// ============================================================================
+
 // re-export protobuf reader for usage by @hashgraph/sdk
 export const Reader = $protobuf.Reader;
 export const Writer = $protobuf.Writer;
+
+// ============================================================================
+// Services
+// ============================================================================
 
 export const ConsensusService = proto.ConsensusService;
 export const CryptoService = proto.CryptoService;
@@ -11,34 +19,43 @@ export const FileService = proto.FileService;
 export const FreezeService = proto.FreezeService;
 export const MirrorConsensusService = proto.MirrorConsensusService;
 export const NetworkService = proto.NetworkService;
+export const ScheduleService = proto.ScheduleService;
 export const SmartContractService = proto.SmartContractService;
 export const TokenService = proto.TokenService;
-export const ScheduleService = proto.ScheduleService;
+
+// ============================================================================
+// Types
+// ============================================================================
 
 export const AccountID = proto.AccountID;
+export const ConsensusTopicInfo = proto.ConsensusTopicInfo;
+export const ConsensusTopicQuery = proto.ConsensusTopicQuery;
+export const ConsensusTopicResponse = proto.ConsensusTopicResponse;
 export const ContractGetInfoResponse = proto.ContractGetInfoResponse;
 export const ContractID = proto.ContractID;
 export const CryptoGetInfoResponse = proto.CryptoGetInfoResponse;
 export const FileGetInfoResponse = proto.FileGetInfoResponse;
 export const FileID = proto.FileID;
+export const Key = proto.Key;
+export const KeyList = proto.KeyList;
 export const NetworkGetVersionInfoResponse = proto.NetworkGetVersionInfoResponse;
 export const Query = proto.Query;
 export const ResponseCodeEnum = proto.ResponseCodeEnum;
 export const ResponseType = proto.ResponseType;
+export const SchedulableTransactionBody = proto.SchedulableTransactionBody;
+export const ScheduleGetInfoResponse = proto.ScheduleGetInfoResponse;
+export const ScheduleID = proto.ScheduleID;
 export const SemanticVersion = proto.SemanticVersion;
+export const SignedTransaction = proto.SignedTransaction;
+export const ThresholdKey = proto.ThresholdKey;
+export const TokenFreezeStatus = proto.TokenFreezeStatus;
 export const TokenID = proto.TokenID;
+export const TokenInfo = proto.TokenInfo;
+export const TokenKycStatus = proto.TokenKycStatus;
 export const TopicID = proto.TopicID;
 export const Transaction = proto.Transaction;
-export const SignedTransaction = proto.SignedTransaction;
-export const TransactionList = proto.TransactionList;
 export const TransactionBody = proto.TransactionBody;
 export const TransactionID = proto.TransactionID;
+export const TransactionList = proto.TransactionList;
 export const TransactionReceipt = proto.TransactionReceipt;
 export const TransactionRecord = proto.TransactionRecord;
-export const ConsensusTopicResponse = proto.ConsensusTopicResponse;
-export const ConsensusTopicQuery = proto.ConsensusTopicQuery;
-export const Key = proto.Key;
-export const KeyList = proto.KeyList;
-export const ThresholdKey = proto.ThresholdKey;
-export const ScheduleID = proto.ScheduleID;
-export const SchedulableTransactionBody = proto.SchedulableTransactionBody;
