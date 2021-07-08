a661b63d89c7650e5e1454f5875512f947c004f5
andvara-naut <dalton.kaua@launchbadge.com>
2021-07-08 16:40:22 -0700

    feat: support file parsing 0.0.111
    Signed-off-by: andvara-naut <dalton.kaua@launchbadge.com>

--- src/TransactionFeeSchedule.js
+++ src/TransactionFeeSchedule.js
@@ -1,12 +1,13 @@
 import * as proto from "@hashgraph/proto";
+
 export default class TransactionFeeSchedule {
     /**
-    * @param {object} props
-    * @param {RequestType} props.hederaFunctionality
-    * @param {FeeData} props.feeData
+    * @param {object} [props]
+    * @param {RequestType} [props.hederaFunctionality]
+    * @param {FeeData} [props.feeData]
     */
    
-    constructor(props) {
+    constructor(props = {}) {
         /*
         * A particular transaction or query
         *
@@ -27,14 +28,15 @@ export default class TransactionFeeSchedule {
      * @returns {TransactionFeeSchedule}
     */
     static fromBytes(bytes) {
-        return TransactionFeeSchedule.fromProtobuf(proto.TransactionFeeSchedule.decode(bytes));
+        return TransactionFeeSchedule._fromProtobuf(proto.TransactionFeeSchedule.decode(bytes));
     }
 
     /**
+    * @internal
     * @param {proto.ITransactionFeeSchedule} transactionFeeSchedule
     * @returns {TransactionFeeSchedule}
     */
-    static fromProtobuf(transactionFeeSchedule) {
+    static _fromProtobuf(transactionFeeSchedule) {
         return new TransactionFeeSchedule({
             /** @type {RequestType} */
             hederaFunctionality: (transactionFeeSchedule.hederaFunctionality),
@@ -44,9 +46,10 @@ export default class TransactionFeeSchedule {
     }
 
    /**
+    * @internal
      * @returns {proto.ITransactionFeeSchedule}
      */
-    toProtobuf() {
+    _toProtobuf() {
         return {
             hederaFunctionality: this.hederaFunctionality,
             feeData: this.feeData,
@@ -57,6 +60,6 @@ export default class TransactionFeeSchedule {
      * @returns {Uint8Array}
      */
     toBytes(){
-       return proto.TransactionFeeSchedule.encode(this.toProtobuf()).finish();    
+       return proto.TransactionFeeSchedule.encode(this._toProtobuf()).finish();    
     }
 }
