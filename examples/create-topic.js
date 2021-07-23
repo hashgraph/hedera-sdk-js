1c93bcb89a339cdda4fd64cdee2cc84c2df29d4b
andvara-naut <dalton.kaua@launchbadge.com>
2021-07-23 13:02:28 -0700

    feat: examples should construct the client more cleanly
    Signed-off-by: andvara-naut <dalton.kaua@launchbadge.com>

--- examples/create-topic.js
+++ examples/create-topic.js
@@ -11,26 +11,13 @@ const {
 async function main() {
     let client;
 
-    if (process.env.HEDERA_NETWORK != null) {
-        if (process.env.OPERATOR_KEY != null) {
-            if (process.env.OPERATOR_ID != null) {
-                client = Client.forName(process.env.HEDERA_NETWORK);   
-                
-                const operatorKey = PrivateKey.fromString(process.env.OPERATOR_KEY);
-                const operatorId = AccountId.fromString(process.env.OPERATOR_ID);
-        
-                client.setOperator(operatorId, operatorKey);
-            }
-            else {
-                throw new Error("Environment Variable:\tOPERATOR_ID is required to initialize the client.");
-            }
-        }
-        else {
-            throw new Error("Environment Variable:\tOPERATOR_KEY is required to initialize the client.");
-        }
-    }
-    else {
-        throw new Error("Environment Variable:\tHEDERA_NETWORK is required to initialize the client.\nThis value can be \"mainnet\", \"testnet\", or \"previewnet\".");
+    try {
+        client = Client.forName(process.env.HEDERA_NETWORK).setOperator(
+            AccountId.fromString(process.env.OPERATOR_ID), 
+            PrivateKey.fromString(process.env.OPERATOR_KEY)
+            );            
+    } catch {
+        throw new Error("Environment variables HEDERA_NETWORK, OPERATOR_ID, and OPERATOR_KEY are required.")
     }
 
     // create topic
