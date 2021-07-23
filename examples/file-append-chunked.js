075c4f034b48dcbe919aa30ff28316a38bf2fdf9
andvara-naut <dalton.kaua@launchbadge.com>
2021-07-23 11:37:55 -0700

    feat:  examples should construct the client more cleanly
    Signed-off-by: andvara-naut <dalton.kaua@launchbadge.com>

--- examples/file-append-chunked.js
+++ examples/file-append-chunked.js
@@ -62,26 +62,25 @@ async function main() {
     let client;
 
     if (process.env.HEDERA_NETWORK != null) {
-        switch (process.env.HEDERA_NETWORK) {
-            case "previewnet":
-                client = Client.forPreviewnet();
-                break;
-            default:
-                client = Client.forTestnet();
+        if (process.env.OPERATOR_KEY != null) {
+            if (process.env.OPERATOR_ID != null) {
+                client = Client.forName(process.env.HEDERA_NETWORK);   
+                
+                const operatorKey = PrivateKey.fromString(process.env.OPERATOR_KEY);
+                const operatorId = AccountId.fromString(process.env.OPERATOR_ID);
+        
+                client.setOperator(operatorId, operatorKey);
+            }
+            else {
+                throw new Error("Environment Variable:\tOPERATOR_ID is required to initialize the client.");
+            }
         }
-    } else {
-        try {
-            client = await Client.fromConfigFile(process.env.CONFIG_FILE);
-        } catch (err) {
-            client = Client.forTestnet();
+        else {
+            throw new Error("Environment Variable:\tOPERATOR_KEY is required to initialize the client.");
         }
     }
-
-    if (process.env.OPERATOR_KEY != null && process.env.OPERATOR_ID != null) {
-        const operatorKey = PrivateKey.fromString(process.env.OPERATOR_KEY);
-        const operatorId = AccountId.fromString(process.env.OPERATOR_ID);
-
-        client.setOperator(operatorId, operatorKey);
+    else {
+        throw new Error("Environment Variable:\tHEDERA_NETWORK is required to initialize the client.\nThis value can be \"mainnet\", \"testnet\", or \"previewnet\".");
     }
 
     const resp = await new FileCreateTransaction()
