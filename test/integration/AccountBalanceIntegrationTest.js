64eb8bbf640b21930899eb7500720a27cdb8c23f
Daniel Akhterov <36461294+danielakhterov@users.noreply.github.com>
2021-10-15 13:47:21 -0700

    feat: timeout grpc's connection manually since it doesn't actually us… (#671)
    * feat: timeout grpc's connection manually since it doesn't actually use the timeout_ms option
    
    Signed-off-by: Daniel Akhterov <daniel@launchbadge.com>
    
    * fix: `Query._advanceRequest()` not incrementing node index for free queries and update timeout test
    
    Signed-off-by: Daniel Akhterov <daniel@launchbadge.com>
    
    * chore: run `pnpm format && pnpm lint`
    
    Signed-off-by: Daniel Akhterov <daniel@launchbadge.com>
    
    * chore: update pnpm lock file
    
    Signed-off-by: Daniel Akhterov <daniel@launchbadge.com>

--- test/integration/AccountBalanceIntegrationTest.js
+++ test/integration/AccountBalanceIntegrationTest.js
@@ -2,8 +2,9 @@ import {
     Status,
     AccountBalanceQuery,
     TokenCreateTransaction,
+    AccountId,
 } from "../src/exports.js";
-import IntegrationTestEnv from "./client/NodeIntegrationTestEnv.js";
+import IntegrationTestEnv, { Client } from "./client/NodeIntegrationTestEnv.js";
 
 describe("AccountBalanceQuery", function () {
     it("an account that does not exist should return an error", async function () {
@@ -26,6 +27,37 @@ describe("AccountBalanceQuery", function () {
         }
 
         await env.close();
+    });
+
+    it("should error", async function () {
+        this.timeout(60000);
+
+        const client = Client.forNetwork({
+            "35.237.200.180:50211": new AccountId(3),
+            "35.242.233.154:50211": new AccountId(10),
+        });
+
+        let err = false;
+
+        try {
+            await new AccountBalanceQuery()
+                .setMaxAttempts(3)
+                .setNodeAccountIds([new AccountId(10)])
+                .setAccountId(new AccountId(10))
+                .execute(client);
+        } catch (error) {
+            err = error.toString().includes("UNAVAILABLE");
+        }
+
+        if (!err) {
+            throw new Error("AccountBalanceQuery on node 10 did not error");
+        }
+
+        await new AccountBalanceQuery()
+            .setMaxAttempts(3)
+            .setNodeAccountIds([new AccountId(10), new AccountId(3)])
+            .setAccountId(new AccountId(10))
+            .execute(client);
     });
 
     it("should reflect token with no keys", async function () {
