c4eebd86409967996183c3da208a8ed1f16fabd0
Daniel Akhterov <daniel@launchbadge.com>
2021-10-26 22:44:09 -0700

    chore: add comments to contract examples
    Signed-off-by: Daniel Akhterov <daniel@launchbadge.com>

--- examples/create-simple-contract.js
+++ examples/create-simple-contract.js
@@ -11,6 +11,7 @@ const {
     AccountId,
 } = require("@hashgraph/sdk");
 
+// Import the compiled contract
 const helloWorld = require("./hello_world.json");
 
 async function main() {
@@ -27,32 +28,53 @@ async function main() {
         );
     }
 
+    // The contract bytecode is located on the `object` field
     const contractByteCode = helloWorld.object;
 
+    // Create a file on Hedera which contains the contact bytecode.
+    // Note: The contract bytecode **must** be hex encoded, it should not
+    // be the actual data the hex represents
     const fileTransactionResponse = await new FileCreateTransaction()
         .setKeys([client.operatorPublicKey])
         .setContents(contractByteCode)
         .execute(client);
 
+    // Fetch the receipt for transaction that created the file
     const fileReceipt = await fileTransactionResponse.getReceipt(client);
+
+    // The file ID is located on the transaction receipt
     const fileId = fileReceipt.fileId;
 
     console.log(`contract bytecode file: ${fileId}`);
 
+    // Create the contract
     const contractTransactionResponse = await new ContractCreateTransaction()
+        // Set gas to create the contract
         .setGas(75000)
+        // The contract bytecode must be set to the file ID containing the contract bytecode
         .setBytecodeFileId(fileId)
+        // Set the admin key on the contract in case the contract should be deleted or
+        // updated in the future
         .setAdminKey(client.operatorPublicKey)
         .execute(client);
 
+    // Fetch the receipt for the transaction that created the contract
     const contractReceipt = await contractTransactionResponse.getReceipt(client);
+
+    // The conract ID is located on the transaction receipt
     const contractId = contractReceipt.contractId;
 
     console.log(`new contract ID: ${contractId}`);
 
+    // Call a method on a contract that exists on Hedera
+    // Note: `ContractCallQuery` cannot mutate a contract, it will only return the last state
+    // of the contract
     const contractCallResult = await new ContractCallQuery()
+        // Set the gas to execute a contract call
         .setGas(75000)
+        // Set which contract
         .setContractId(contractId)
+        // Set the function to call on the contract
         .setFunction("greet")
         .setQueryPayment(new Hbar(1))
         .execute(client);
@@ -61,6 +83,15 @@ async function main() {
         console.log(`error calling contract: ${contractCallResult.errorMessage}`);
     }
 
+    // Get the message from the result
+    // The `0` is the index to fetch a particular type from
+    //
+    // e.g.
+    // If the return type of `get_message` was `(string[], uint32, string)`
+    // then you'd need to get each field separately using:
+    //      const stringArray = contractCallResult.getStringArray(0);
+    //      const uint32 = contractCallResult.getUint32(1);
+    //      const string = contractCallResult.getString(2);
     const message = contractCallResult.getString(0);
     console.log(`contract message: ${message}`);
 
@@ -68,6 +99,10 @@ async function main() {
         .setContractId(contractId)
         .execute(client);
 
+    // Delete the contract
+    // Note: The admin key of the contract needs to sign the transaction
+    // In this case the client operator is the same as the admin key so the
+    // automatic signing takes care of this for you
     const contractDeleteReceipt = await contractDeleteResult.getReceipt(client);
 
     console.log("contract successfully deleted");
