ccfb79510ac2f43a0f98c7aef138732739b2cc37
Daniel Akhterov <daniel@launchbadge.com>
2021-11-03 15:07:08 -0700

    fix: extract nested trenary statements and remove unused variable
    Signed-off-by: Daniel Akhterov <daniel@launchbadge.com>

--- common_js_test/src/test.js
+++ common_js_test/src/test.js
@@ -4,12 +4,12 @@ const {
 } = require("@hashgraph/sdk");
 
 describe("CommonJS", function () {
-    it("test", async function () {
+    it("it should query each node's balance", async function () {
         this.timeout(30000);
 
         const client = Client.forPreviewnet();
 
-        for (const [_, nodeAccountId] of Object.entries(client.network)) {
+        for (const [, nodeAccountId] of Object.entries(client.network)) {
             await new AccountBalanceQuery()
                 .setNodeAccountIds([nodeAccountId])
                 .setAccountId(nodeAccountId)
