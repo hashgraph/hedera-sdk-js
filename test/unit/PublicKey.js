e7c12daa24adb182f2ad36a44640f3c806fb1735
Jack Ta <jack.th.ta@gmail.com>
2021-11-03 17:18:59 -0700

    fix: `PublicKey.verifyTransaction()` (#727)
    Signed-off-by: Jack Ta <jack.th.ta@gmail.com>

---
+++ test/unit/PublicKey.js
@@ -0,0 +1,33 @@
+import {
+    AccountId,
+    PrivateKey,
+    TransactionId,
+    TransferTransaction,
+} from "../src/exports.js";
+
+describe("PublicKey", function () {
+    it("`verifyTransaction` works", async function () {
+        const key = PrivateKey.generate();
+
+        const transaction = new TransferTransaction()
+            .setTransactionId(TransactionId.generate(new AccountId(5)))
+            .setNodeAccountIds([new AccountId(6)])
+            .addHbarTransfer("0.0.3", -1)
+            .addHbarTransfer("0.0.4", 1)
+            .freeze();
+
+        await transaction.sign(key);
+
+        expect(key.publicKey.verifyTransaction(transaction)).to.be.true;
+
+        const signatures = transaction.getSignatures();
+
+        for (const [nodeAccountId, nodeSignatures] of signatures) {
+            expect(nodeAccountId.toString()).equals("0.0.6");
+
+            for (const [publicKey] of nodeSignatures) {
+                expect(publicKey.verifyTransaction(transaction)).to.be.true;
+            }
+        }
+    });
+});
