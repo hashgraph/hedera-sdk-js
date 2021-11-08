bf7b8c4df19278552f486858decad825507fafca
andvara-naut <dalton.kaua@launchbadge.com>
2021-11-08 14:31:22 -0800

    feat: multi-ip-support
    Signed-off-by: valtyr-naut <dalton.kaua@launchbadge.com>

---
+++ test/unit/Network.js
@@ -0,0 +1,12 @@
+import Network from "../src/client/Network.js";
+
+describe("Network.js",function(){
+
+    it("should support assigning multiple ips per account",function(){
+
+    });
+
+
+
+
+});
