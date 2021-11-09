6506f12c51a2b65591a4bfe2ea23ab914961e8ea
andvara-naut <dalton.kaua@launchbadge.com>
2021-11-09 14:53:36 -0800

    checkpoint
    Signed-off-by: valtyr-naut <dalton.kaua@launchbadge.com>

--- test/unit/Network.js
+++ test/unit/Network.js
@@ -1,8 +1,30 @@
+import Channel from "../src/channel/Channel.js";
 import Network from "../src/client/Network.js";
+import Node from "../src/Node.js";
 
 describe("Network.js",function(){
 
     it("should support assigning multiple ips per account",function(){
+        const props={
+            accountId:"0.0.0",
+            address:"1.2.3.4",
+            channelInitFunction: new Channel()
+        };
+
+
+        const channel = new Channel();
+        const network = new Network("1.1.1",channel);
+        const node = new Node(props);
+
+        // network._addNodeToNetwork()
+
+        console.log(channel);
+        console.log(node);
+        console.log(network);
+
+
+
+
 
     });
 
