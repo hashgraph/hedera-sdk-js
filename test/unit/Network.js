08353c9894bc263d5375f51d00d9dfd630ddebf8
andvara-naut <dalton.kaua@launchbadge.com>
2021-11-12 13:31:56 -0800

    feat: multi-ip-test-comment
    Signed-off-by: valtyr-naut <dalton.kaua@launchbadge.com>

--- test/unit/Network.js
+++ test/unit/Network.js
@@ -1,6 +1,6 @@
-import Channel from "../src/channel/Channel.js";
-import Network from "../src/client/Network.js";
-import Node from "../src/Node.js";
+// import Channel from "../src/channel/Channel.js";
+// import Network from "../src/client/Network.js";
+// import Node from "../src/Node.js";
 
 describe("Network.js", function () {
     it("should support assigning multiple ips per account", function () {
@@ -9,13 +9,10 @@ describe("Network.js", function () {
         //     address: "", //temp ip removed for sonarcloud
         //     channelInitFunction: new Channel(),
         // };
-
         // const channel = new Channel();
         // const network = new Network("1.1.1", channel);
         // const node = new Node(props);
-
         // network._addNodeToNetwork()
-
         // console.log(channel);
         // console.log(node);
         // console.log(network);
