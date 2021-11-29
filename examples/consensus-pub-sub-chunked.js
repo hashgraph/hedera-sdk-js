9e54ec03c5b3d3ad40836da1483234d160055f94
andvara-naut <dalton.kaua@launchbadge.com>
2021-11-29 13:35:14 -0800

    feat: add examples
    Signed-off-by: valtyr-naut <dalton.kaua@launchbadge.com>

--- examples/consensus-pub-sub-chunked.js
+++ examples/consensus-pub-sub-chunked.js
@@ -1,9 +1,54 @@
 require("dotenv").config();
 
 const {
+    Client,
+    PrivateKey,
+    AccountId,
+    TopicCreateTransaction,
 
 } = require("@hashgraph/sdk");
 
 async function main() {
-    
+
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    let client;
+
+    try {
+        client = Client.forName(process.env.HEDERA_NETWORK).setOperator(
+            // Defaults the operator account ID and key such that all generated transactions will be paid for
+            // by this account and be signed by this key
+            AccountId.fromString(process.env.OPERATOR_ID),
+            PrivateKey.fromString(process.env.OPERATOR_KEY)
+        );
+    } catch {
+        throw new Error(
+            "Environment variables HEDERA_NETWORK, OPERATOR_ID, and OPERATOR_KEY are required."
+        );
+    }    
+
+    //generate a submit key to use with the topic
+    let submitKey = PrivateKey.generate();
+
+    //make a new topic ID to use
+    let topicId = new TopicCreateTransaction()
+    .setTopicMemo("hedera-sdk-js/consensus-pub-sub-chunked")
+    .setSubmitKey(submitKey)
+    .execute(client)
+    .getReceipt(client)
+    .topicId;
+
+    if (topicId != null && topicId != undefined){
+        console.log("TopicId: " + topicId);
+
+        //wait for mirror to propagate
+        await sleep(10000);
+
+        //cont
+
+    } else {
+        throw new Error("topicId creation failed.");
+    }
 }
