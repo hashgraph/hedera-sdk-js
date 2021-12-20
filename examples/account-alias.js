ea7ee109200c1038338f3a5cc19f5399e2e5ccaf
Daniel Akhterov <daniel@launchbadge.com>
2021-12-20 14:23:22 -0800

    feat: add alias key example
    Signed-off-by: Daniel Akhterov <daniel@launchbadge.com>

---
+++ examples/account-alias.js
@@ -0,0 +1,119 @@
+require("dotenv").config();
+
+const {
+    Client,
+    PrivateKey,
+    PublicKey,
+    Hbar,
+    AccountId,
+    AccountBalanceQuery,
+    AccountInfoQuery,
+    TransferTransaction,
+} = require("@hashgraph/sdk");
+
+async function main() {
+    let client;
+
+    try {
+        // Defaults the operator account ID and key such that all generated transactions will be paid for
+        // by this account and be signed by this key
+        client = Client.forName(process.env.HEDERA_NETWORK).setOperator(
+            AccountId.fromString(process.env.OPERATOR_ID),
+            PrivateKey.fromString(process.env.OPERATOR_KEY)
+        );
+    } catch {
+        throw new Error(
+            "Environment variables HEDERA_NETWORK, OPERATOR_ID, and OPERATOR_KEY are required."
+        );
+    }
+
+    /*
+     * Hedera supports a form of lazy account creation.
+     *
+     * You can "create" an account by generating a private key, and then deriving the public key,
+     * without any need to interact with the Hedera network.  The public key more or less acts as the user's
+     * account ID.  This public key is an account's aliasKey: a public key that aliases (or will eventually alias)
+     * to a Hedera account.
+     *
+     * An AccountId takes one of two forms: a normal AccountId with a null aliasKey member takes the form 0.0.123,
+     * while an account ID with a non-null aliasKey member takes the form
+     * 0.0.302a300506032b6570032100114e6abc371b82dab5c15ea149f02d34a012087b163516dd70f44acafabf7777
+     * Note the prefix of "0.0." indicating the shard and realm.  Also note that the aliasKey is stringified
+     * as a hex-encoded ASN1 DER representation of the key.
+     *
+     * An AccountId with an aliasKey can be used just like a normal AccountId for the purposes of queries and
+     * transactions, however most queries and transactions involving such an AccountId won't work until Hbar has
+     * been transferred to the aliasKey account.
+     *
+     * There is no record in the Hedera network of an account associated with a given aliasKey
+     * until an amount of Hbar is transferred to the account.  The moment that Hbar is transferred to that aliasKey
+     * AccountId is the moment that that account actually begins to exist in the Hedera ledger.
+     */
+
+    console.log("\"Creating\" a new account");
+
+    const privateKey = PrivateKey.generateED25519();
+    const publicKey = privateKey.getPublicKey();
+
+    // Assuming that the target shard and realm are known.
+    // For now they are virtually always 0 and 0.
+    const aliasAccountId = publicKey.toAccountId(0, 0);
+
+    console.log("New account ID: " + aliasAccountId);
+    console.log("Just the aliasKey: " + aliasAccountId.aliasKey);
+
+    /*
+     * Note that no queries or transactions have taken place yet.
+     * This account "creation" process is entirely local.
+     *
+     * AccountId.fromString() can construct an AccountId with an aliasKey.
+     * It expects a string of the form 0.0.123 in the case of a normal AccountId, or of the form
+     * 0.0.302a300506032b6570032100114e6abc371b82dab5c15ea149f02d34a012087b163516dd70f44acafabf7777
+     * in the case of an AccountId with aliasKey.  Note the prefix of "0.0." to indicate the shard and realm.
+     *
+     * If the shard and realm are known, you may use PublicKey.fromString().toAccountId() to construct the
+     * aliasKey AccountId
+     */
+
+    const fromString = AccountId.fromString("0.0.302a300506032b6570032100114e6abc371b82dab5c15ea149f02d34a012087b163516dd70f44acafabf7777");
+
+    const fromKeyString = PublicKey
+            .fromString("302a300506032b6570032100114e6abc371b82dab5c15ea149f02d34a012087b163516dd70f44acafabf7777")
+            .toAccountId(0, 0);
+
+
+    console.log("Transferring some Hbar to the new account");
+    new TransferTransaction()
+        .addHbarTransfer(OPERATOR_ID, new Hbar(10).negated())
+        .addHbarTransfer(aliasAccountId, new Hbar(10))
+        .execute(client)
+        .getReceipt(client);
+
+    const balance = new AccountBalanceQuery()
+        .setAccountId(aliasAccountId)
+        .execute(client);
+
+    console.log("Balances of the new account: " + balance);
+
+    const info = new AccountInfoQuery()
+        .setAccountId(aliasAccountId)
+        .execute(client);
+
+    console.log("Info about the new account: " + info);
+
+    /*
+     * Note that once an account exists in the ledger, it is assigned a normal AccountId, which can be retrieved
+     * via an AccountInfoQuery.
+     *
+     * Users may continue to refer to the account by its aliasKey AccountId, but they may also
+     * now refer to it by its normal AccountId
+     */
+
+    console.log("The normal account ID: " + info.accountId);
+    console.log("The alias key: " + info.aliasKey);
+
+    console.log("Example complete!");
+    client.close();
+}
+
+void main();
