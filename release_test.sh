441fa91a794179a7c4ddffc2b3cc9e78180d0105
Daniel Akhterov <daniel@launchbadge.com>
2022-01-18 20:09:36 -0800

    feat: add simple script to run some checks before release
    Signed-off-by: Daniel Akhterov <daniel@launchbadge.com>

---
+++ release_test.sh
@@ -0,0 +1,22 @@
+#!/bin/env bash
+
+if [ "$(git diff --name-only | wc -l)" -gt 0 ]; then
+    echo "Uncommited changes"
+    exit 1
+fi
+
+pnpm i && pnpm format && pnpm lint
+pnpm test:unit:node
+
+pushd examples
+pnpm i && pnpm format && pnpm lint
+popd
+
+yalc publish
+
+pushd common_js_test
+yalc add @hashgraph/sdk
+pnpm i
+pnpm test
+rm yalc.lock
+popd
