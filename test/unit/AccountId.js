e657782302cdf7120fbef691f50e10f97105ab14
Daniel Akhterov <daniel@launchbadge.com>
2022-02-06 14:50:41 -0800

    fix: remove commented out checksum tests as they've been moved into a separate file
    Signed-off-by: Daniel Akhterov <daniel@launchbadge.com>

--- test/unit/AccountId.js
+++ test/unit/AccountId.js
@@ -72,42 +72,6 @@ describe("AccountId", function () {
         expect(accountId.shard.toNumber()).to.eql(10);
     });
 
-//     it("should parse mainnet ID with checksum {0.0.123-vfmkw}", function () {
-//         const accountId = AccountId.fromString("0.0.123-vfmkw");
-// 
-//         expect(accountId.num.toNumber()).to.eql(123);
-//         expect(accountId.realm.toNumber()).to.eql(0);
-//         expect(accountId.shard.toNumber()).to.eql(0);
-// 
-//         expect(accountId.toStringWithChecksum(Client.forMainnet())).to.be.eql(
-//             "0.0.123-vfmkw"
-//         );
-//     });
-// 
-//     it("should parse testnet ID with checksum {0.0.123-rmkyk}", function () {
-//         const accountId = AccountId.fromString("0.0.123-rmkyk");
-// 
-//         expect(accountId.num.toNumber()).to.eql(123);
-//         expect(accountId.realm.toNumber()).to.eql(0);
-//         expect(accountId.shard.toNumber()).to.eql(0);
-// 
-//         expect(accountId.toStringWithChecksum(Client.forTestnet())).to.be.eql(
-//             "0.0.123-rmkyk"
-//         );
-//     });
-// 
-//     it("should parse previewnet ID with checksum {0.0.123-ntjly}", function () {
-//         const accountId = AccountId.fromString("0.0.123-ntjly");
-// 
-//         expect(accountId.num.toNumber()).to.eql(123);
-//         expect(accountId.realm.toNumber()).to.eql(0);
-//         expect(accountId.shard.toNumber()).to.eql(0);
-// 
-//         expect(
-//             accountId.toStringWithChecksum(Client.forPreviewnet())
-//         ).to.be.eql("0.0.123-ntjly");
-//     });
-// 
     it("should parse 0.0.0", function () {
         const accountId = AccountId.fromString("0.0.0");
 
