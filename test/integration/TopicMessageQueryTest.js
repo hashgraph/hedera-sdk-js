7589304050b41be7d82cfe66e6a5e23cf0bf2f60
Daniel Akhterov <daniel@launchbadge.com>
2022-02-13 12:28:31 -0800

    fix: mirror network incorrectly using port 433 instead of 443
    Signed-off-by: Daniel Akhterov <daniel@launchbadge.com>

---
+++ test/integration/TopicMessageQueryTest.js
@@ -0,0 +1,33 @@
+import { TopicMessageQuery } from "../../src/exports.js";
+import { Client } from "./client/NodeIntegrationTestEnv.js";
+
+describe("TopicMessageQuery", function () {
+    it("should be executable", async function () {
+        this.timeout(10000);
+
+        const client = Client.forNetwork({});
+        client.setTransportSecurity(true);
+        client.setMirrorNetwork(["mainnet-public.mirrornode.hedera.com:443"]);
+        client.mirroNetwork();
+
+        let finished = false;
+        let endTime = Date.now() + 5000;
+
+        new TopicMessageQuery()
+            .setTopicId("0.0.120438")
+            .setStartTime(0)
+            .setLimit(1)
+            // eslint-disable-next-line no-unused-vars
+            .subscribe(client, (_) => {
+                finished = true;
+            });
+
+        while (!finished && Date.now() < endTime) {
+            await new Promise((resolved) => setTimeout(resolved, 5000));
+        }
+
+        if (!finished) {
+            throw new Error("Did not receive message from query");
+        }
+    });
+});
