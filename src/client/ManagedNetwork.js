a7d618fe9105a553b24988c09b0640100b5956c5
Victor ADĂSCĂLIȚEI <210908+3Nigma@users.noreply.github.com>
2022-02-21 05:33:30 +0200

    Fix Transaction.freezeWith followed by PrivateKey.signTransaction exception scenario  (#953)
    * fix: update changelog with allowances feature
    
    Signed-off-by: Daniel Akhterov <daniel@launchbadge.com>
    
    * Hip 336 issue 923 (#924)
    
    * Added missing fields.
    
    * Removed debug log from pnpm.
    
    * fix: make `owner` null-able and fix package links
    
    Signed-off-by: Daniel Akhterov <daniel@launchbadge.com>
    
    Co-authored-by: Daniel Akhterov <daniel@launchbadge.com>
    
    * feat: implement grpc deadline and request timeouts
    
    Signed-off-by: Daniel Akhterov <daniel@launchbadge.com>
    
    * added new status codes to Status.js (#936)
    
    Signed-off-by: keirabee <keira.black@launchbadge.com>
    
    * fix: `@hashgraph/proto` should depend on `long` and force `@protobufsjs/minimal` to use `Long` internally (#935)
    
    Signed-off-by: Daniel Akhterov <daniel@launchbadge.com>
    
    * feat: implement `AddressBookQuery` (#934)
    
    Signed-off-by: Daniel Akhterov <daniel@launchbadge.com>
    
    * Fix browser unit tests (#925)
    
    * fix(wip): browser tests
    
    Signed-off-by: Daniel Akhterov <daniel@launchbadge.com>
    
    * fix: remove commented out checksum tests as they've been moved into a separate file
    
    Signed-off-by: Daniel Akhterov <daniel@launchbadge.com>
    
    * fix: update github actions to run unit tests in browser instead of integration
    
    Signed-off-by: Daniel Akhterov <daniel@launchbadge.com>
    
    * fix: re-add `test/.eslintrc.cjs` file and fix formatting
    
    Signed-off-by: Daniel Akhterov <daniel@launchbadge.com>
    
    * fix: add `DelegateContractId` to exports
    
    Signed-off-by: Daniel Akhterov <daniel@launchbadge.com>
    
    * Added example file hts-nftP1-fee-create-mint-burn-associate-transfer.js to support blog post on NFTs with HTS (#708)
    
    * Added nft examples/hts-nftP1-fee-create-mint-burn-associate-transfer.js
    
    * Added parts 2 and 3 of NFT examples to the examples folder.
    
    * fix: `ContractId.fromString()` with a `num` should correctly parse
    
    Signed-off-by: Daniel Akhterov <daniel@launchbadge.com>
    
    * fix: mirror network incorrectly using port 433 instead of 443
    
    Signed-off-by: Daniel Akhterov <daniel@launchbadge.com>
    
    * fix: new examples should use JSDoc TypeScript
    
    Signed-off-by: Daniel Akhterov <daniel@launchbadge.com>
    
    * fix: `TransactionReceipt._toProtobuf()` incorrectly encoding `tokenId` and `topicId`
    
    Signed-off-by: Daniel Akhterov <daniel@launchbadge.com>
    
    * fix: `TransactionReceipt._toProtobuf()` incorrectly encoding `tokenId` and `topicId`
    
    Signed-off-by: Daniel Akhterov <daniel@launchbadge.com>
    Signed-off-by: Victor ADASCALITEI <210908+3Nigma@users.noreply.github.com>
    
    * Added actual fix concerning ManagedNetwork._getNumberOfMostHealthyNodes
    
    Signed-off-by: Victor ADASCALITEI <210908+3Nigma@users.noreply.github.com>
    
    * Reverted leaked commit.
    
    Signed-off-by: Victor ADASCALITEI <210908+3Nigma@users.noreply.github.com>
    
    Co-authored-by: Daniel Akhterov <daniel@launchbadge.com>
    Co-authored-by: keirabee <97643903+keirabee@users.noreply.github.com>
    Co-authored-by: Daniel Akhterov <36461294+danielakhterov@users.noreply.github.com>
    Co-authored-by: ed-marquez <72571340+ed-marquez@users.noreply.github.com>
    Co-authored-by: Victor ADĂSCĂLIȚEI <victor.adascalitei@buidlerlabs.com>

--- src/client/ManagedNetwork.js
+++ src/client/ManagedNetwork.js
@@ -203,6 +203,7 @@ export default class MangedNetwork {
             }
 
             if (!keys.has(node.getKey())) {
+                keys.add(node.getKey());
                 nodes.push(node);
             }
         }
