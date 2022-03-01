906f1a75f8f6e8401df07501251a848a3a097e81
Daniel Akhterov <daniel@launchbadge.com>
2022-02-28 16:03:05 -0800

    test: add simple test for `AccountInfoQuery.[to|from]Bytes()`
    Signed-off-by: Daniel Akhterov <daniel@launchbadge.com>

---
+++ test/unit/AccountInfoQuery.js
@@ -0,0 +1,15 @@
+import { AccountId, Query, AccountInfoQuery } from "../../src/exports.js";
+
+describe("AccountInfo", function () {
+    it("[to|from]Bytes()", async function () {
+        const accountId = new AccountId(10);
+
+        const query = Query.fromBytes(
+            new AccountInfoQuery().setAccountId(accountId).toBytes()
+        );
+
+        expect(query instanceof AccountInfoQuery).to.be.true;
+
+        expect(query.accountId.toString()).to.be.equal(accountId.toString());
+    });
+});
