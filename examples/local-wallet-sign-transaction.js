1474b9630955bd3e0d0012ee0609fbde65a7b5ef
Daniel Akhterov <daniel@launchbadge.com>
2022-03-08 01:45:33 -0800

    fix: wallet example should use WithSigner methods
    Signed-off-by: Daniel Akhterov <daniel@launchbadge.com>

--- examples/local-wallet-sign-transaction.js
+++ examples/local-wallet-sign-transaction.js
@@ -7,13 +7,13 @@ dotenv.config();
 async function main() {
     const wallet = new LocalWallet();
 
-    let transaction = new TransferTransaction()
+    let transaction = await new TransferTransaction()
         .addHbarTransfer("0.0.3", 1)
-        .addHbarTransfer(wallet.getAccountId(), -1);
+        .addHbarTransfer(wallet.getAccountId(), -1)
+        .freezeWithSigner(wallet);
 
-    const populatedTransaction = await wallet.populateTransaction(transaction);
-    const signedTransaction = await wallet.signTransaction(populatedTransaction);
-    const response = await wallet.sendRequest(signedTransaction);
+    transaction = await transaction.signWithSigner(wallet);
+    const response = await transaction.executeWithSigner(wallet);
     const receipt = await wallet.getProvider().waitForReceipt(response);
 
     console.log(`status: ${receipt.status.toString()}`);
