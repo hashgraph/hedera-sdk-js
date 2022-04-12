84bc670381198fbab2b64343d18ede90583863d7
Daniel Akhterov <daniel@launchbadge.com>
2022-04-11 20:40:46 -0700

    fix: remove `env` from scripts
    Signed-off-by: Daniel Akhterov <daniel@launchbadge.com>

--- test/integration/LocalWalletTest.js
+++ test/integration/LocalWalletTest.js
@@ -1,10 +1,15 @@
-import { LocalWallet } from "../../src/index.js";
+import { Wallet } from "../../src/index.js";
 
 describe("LocalWallet", function () {
     it("can fetch wallet's info", async function () {
         this.timeout(120000);
 
-        const wallet = new LocalWallet();
+        const wallet = new Wallet(
+            process.env.OPERATOR_ID,
+            process.env.OPERATOR_KEY,
+            new LocalProvider()
+        );
+
         const info = await wallet.getAccountInfo();
 
         expect(info.accountId.compare(wallet.getAccountId())).to.be.equal(0);
