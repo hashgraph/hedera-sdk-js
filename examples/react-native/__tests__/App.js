d4798d9d1532217a402b92f3d5ec58054ad74947
Daniel Akhterov <daniel@launchbadge.com>
2022-04-13 17:09:41 -0700

    fix: re-add existing `react-native` example
    Signed-off-by: Daniel Akhterov <daniel@launchbadge.com>

---
+++ examples/react-native/__tests__/App.js
@@ -0,0 +1,9 @@
+import 'react-native';
+import 'react';
+import '../App';
+
+// Note: test renderer must be required after react-native.
+import 'react-test-renderer';
+
+it('renders correctly', () => {
+});
