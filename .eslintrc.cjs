63688abd628a23aa7796fc8bac3b899cf15d3781
Daniel Akhterov <daniel@launchbadge.com>
2022-04-17 18:37:13 -0700

    chore: run `pnpm format`
    Signed-off-by: Daniel Akhterov <daniel@launchbadge.com>

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -24,7 +24,7 @@ module.exports = {
         sourceType: "module",
         warnOnUnsupportedTypeScriptVersion: false,
     },
-    plugins: ["@typescript-eslint", "deprecation", "ie11", "license-header"],
+    plugins: ["@typescript-eslint", "deprecation", "ie11"],
     rules: {
         // does not handle return types being annotated in a type comment
         "@typescript-eslint/explicit-function-return-type": "off",
@@ -66,7 +66,6 @@ module.exports = {
         "ie11/no-collection-args": "error",
         "ie11/no-for-in-const": "error",
         "ie11/no-loop-func": "warn",
-        "ie11/no-weak-collections": "error",
-        "license-header/header": [ "error", "./resources/license-header.js" ],
+        "ie11/no-weak-collections": "error"
     },
 };
