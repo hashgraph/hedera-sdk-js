322752cf9ba9786f85d2239d0420b92450bfc2b7
Daniel Akhterov <akhterovd@gmail.com>
2022-08-02 14:13:42 -0700

    feat: export const paths
    Signed-off-by: Daniel Akhterov <akhterovd@gmail.com>

--- packages/cryptography/src/index.js
+++ packages/cryptography/src/index.js
@@ -2,7 +2,7 @@ export { default as Key } from "./Key.js";
 export { default as KeyList } from "./KeyList.js";
 export { default as PrivateKey } from "./PrivateKey.js";
 export { default as PublicKey } from "./PublicKey.js";
-export { default as Mnemonic } from "./Mnemonic.js";
+export { default as Mnemonic, HEDERA_PATH, SLIP44_ECDSA_ETH_PATH, SLIP44_ECDSA_HEDERA_PATH } from "./Mnemonic.js";
 export { default as BadKeyError } from "./BadKeyError.js";
 export { default as BadMnemonicError } from "./BadMnemonicError.js";
 export { default as BadMnemonicReason } from "./BadMnemonicReason.js";
