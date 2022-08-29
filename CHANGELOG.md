b587185b183fd922e78e9d10d2fbac51522bf285
Daniel Akhterov <daniel@launchbadge.com>
2022-08-29 11:52:42 -0700

    fix: `ContratCrateFlow.execute()` should sign all transations with any key passed to `.sign()`
    Signed-off-by: Daniel Akhterov <daniel@launchbadge.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,6 +8,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ### Fixed
 
+ * `ContractCreateFlow.execute()` not using keys provided in `.sign()`
  * `PublicKey.equals()`
 
 ## v2.18.0
---
+++ test/unit/ContractCreateFlowMocking.js
@@ -0,0 +1,61 @@
+import { expect } from "chai";
+import { ContractCreateFlow, Transaction, PrivateKey } from "../../src/index.js";
+import Mocker, { TRANSACTION_RECEIPT_SUCCESS_RESPONSE } from "./Mocker.js";
+import { proto } from "@hashgraph/proto";
+import { bigContents } from "../integration/contents.js";
+
+describe("ContractCreateFlowMocking", function () {
+    let client;
+    let servers;
+
+    afterEach(function () {
+        client.close();
+        servers.close();
+    });
+
+    it("signs all transactions", async function () {
+        this.timeout(10000);
+
+        const key = PrivateKey.generate();
+
+        const verifyTransactionCall = (request) => {
+            const transaction = Transaction.fromBytes(proto.Transaction.encode({
+                signedTransactionBytes: request.signedTransactionBytes
+            }).finish());
+            
+            expect(key.publicKey.verifyTransaction(transaction)).to.be.true;
+
+            return { nodeTransactionPrecheckCode: proto.ResponseCodeEnum.OK };
+        };
+
+        ({ client, servers } = await Mocker.withResponses([
+            [
+                { call: verifyTransactionCall },
+                { response: TRANSACTION_RECEIPT_SUCCESS_RESPONSE },
+                { call: verifyTransactionCall },
+                { response: TRANSACTION_RECEIPT_SUCCESS_RESPONSE },
+                { call: verifyTransactionCall },
+                { response: TRANSACTION_RECEIPT_SUCCESS_RESPONSE },
+                { call: verifyTransactionCall },
+                { response: TRANSACTION_RECEIPT_SUCCESS_RESPONSE },
+                { call: verifyTransactionCall },
+                { response: TRANSACTION_RECEIPT_SUCCESS_RESPONSE },
+                { call: verifyTransactionCall },
+                { response: TRANSACTION_RECEIPT_SUCCESS_RESPONSE },
+                { call: verifyTransactionCall },
+                { response: TRANSACTION_RECEIPT_SUCCESS_RESPONSE },
+                { call: verifyTransactionCall },
+                { response: TRANSACTION_RECEIPT_SUCCESS_RESPONSE },
+                { call: verifyTransactionCall },
+                { response: TRANSACTION_RECEIPT_SUCCESS_RESPONSE },
+                { call: verifyTransactionCall },
+                { response: TRANSACTION_RECEIPT_SUCCESS_RESPONSE },
+            ],
+        ]));
+
+        await new ContractCreateFlow()
+            .setBytecode(bigContents)
+            .sign(key)
+            .execute(client);
+    });
+});
