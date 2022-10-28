1d1fcfe91ac8804c05c271271ff9ad3b51975a16
ochikov <ognyan@limechain.tech>
2022-10-28 12:34:28 +0300

    extend timeout of browser test
    Signed-off-by: ochikov <ognyan@limechain.tech>

--- packages/cryptography/test/browser.test.js
+++ packages/cryptography/test/browser.test.js
@@ -1,22 +1,29 @@
 import { test, expect } from "@playwright/test";
 
-test("can execute @hashgraph/cryptography within browser", async function ({ page }) {
+test("can execute @hashgraph/cryptography within browser", async function ({
+    page,
+}) {
     page.on("pageerror", async (message) => {
         throw new Error(message);
     });
 
     await page.goto("http://localhost:9001/");
 
-    await new Promise((resolve) => setTimeout(resolve, 2000));
+    await new Promise((resolve) => setTimeout(resolve, 3000));
     const tests = await page.evaluate(() => {
-        return Array.from(document.querySelectorAll(".test"))
-            .map((test) => {
-                const error = test.querySelector(".error");
-                return {
-                    name: [].reduce.call(test.querySelector("h2").childNodes, function(a, b) { return a + (b.nodeType === 3 ? b.textContent : ''); }, ''),
-                    error: error != null ? error.innerText.trim() : null,
-                };
-            });
+        return Array.from(document.querySelectorAll(".test")).map((test) => {
+            const error = test.querySelector(".error");
+            return {
+                name: [].reduce.call(
+                    test.querySelector("h2").childNodes,
+                    function (a, b) {
+                        return a + (b.nodeType === 3 ? b.textContent : "");
+                    },
+                    ""
+                ),
+                error: error != null ? error.innerText.trim() : null,
+            };
+        });
     });
 
     expect(tests.length).toBeGreaterThan(0);
