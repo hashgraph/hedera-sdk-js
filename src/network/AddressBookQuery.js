aa11eeacd5b887bd29615fc8a8d8971c465a5b60
ochikov <ochikov@users.noreply.github.com>
2022-12-19 18:47:28 +0200

    Update the addressbook update to be 24 hours (#1359)
    * Update the addressbook update to be 24 hours
    
    Signed-off-by: ochikov <ognyan@limechain.tech>
    
    * Fix the common_js tests
    
    Signed-off-by: ochikov <ognyan@limechain.tech>
    
    Signed-off-by: ochikov <ognyan@limechain.tech>

--- src/network/AddressBookQuery.js
+++ src/network/AddressBookQuery.js
@@ -224,10 +224,6 @@ export default class AddressBookQuery {
                     const message =
                         error instanceof Error ? error.message : error.details;
                     console.log("ERROR:", error);
-                    console.log(
-                        "ERRORED, but CLIENT CLOSE IS",
-                        client.isClientShutDown
-                    );
                     if (
                         this._attempt < this._maxAttempts &&
                         !client.isClientShutDown &&
