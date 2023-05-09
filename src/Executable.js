a865fddf0d1662b8c4de336b38adaafeb7a86de6
Petar Tonev <petar.tonev@limechain.tech>
2023-05-09 23:06:50 +0300

    add logger debug info about unhealthy nodes
    Signed-off-by: Petar Tonev <petar.tonev@limechain.tech>

--- src/Executable.js
+++ src/Executable.js
@@ -651,6 +651,9 @@ export default class Executable {
                 ) {
                     // Increase the backoff for the particular node and remove it from
                     // the healthy node list
+                    Logger.debug(
+                        `Node with accountId: ${node.accountId} and IP: ${client.network.address}`
+                    );
                     client._network.increaseBackoff(node);
                     continue;
                 }
