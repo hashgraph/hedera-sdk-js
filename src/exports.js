02924121f0849002455b1863adbc7839a7defae4
Petar Tonev <petar.tonev@limechain.tech>
2023-07-05 12:15:48 +0300

    edit readme and provide tips and tricks
    Signed-off-by: Petar Tonev <petar.tonev@limechain.tech>

--- src/exports.js
+++ src/exports.js
@@ -20,14 +20,14 @@
 import * as EntityIdHelper from "./EntityIdHelper.js";
 
 export { default as Cache } from "./Cache.js";
-/* export {
+export {
     BadKeyError,
     BadMnemonicError,
     BadMnemonicReason,
     HEDERA_PATH,
     SLIP44_ECDSA_ETH_PATH,
     SLIP44_ECDSA_HEDERA_PATH,
-} from "@hashgraph/cryptography"; */
+} from "@hashgraph/cryptography";
 export { default as PrivateKey } from "./PrivateKey.js";
 export { default as PublicKey } from "./PublicKey.js";
 export { default as KeyList } from "./KeyList.js";
