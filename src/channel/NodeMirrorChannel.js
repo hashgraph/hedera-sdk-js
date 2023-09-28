7e9dedc0c9d5676c5b1ea2bfde5d0fe9ecec9121
Petar Tonev <petar.tonev@limechain.tech>
2023-09-28 17:15:13 +0300

    after task build
    Signed-off-by: Petar Tonev <petar.tonev@limechain.tech>

--- src/channel/NodeMirrorChannel.js
+++ src/channel/NodeMirrorChannel.js
@@ -99,7 +99,7 @@ export default class NodeMirrorChannel extends MirrorChannel {
             })
             // eslint-disable-next-line @typescript-eslint/no-unused-vars
             .on("error", (/** @type {grpc.StatusObject} */ err) => {
-                error(err)
+                error(err);
             });
 
         return () => {
