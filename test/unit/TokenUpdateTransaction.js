5fffabefd3008171329514c0d228590526580a24
svetoslav-nikol0v <svetoslav.nikolov@limechain.tech>
2024-04-24 18:51:42 +0300

    feature: update metadata field of fungible and non-fungible tokens, and dynamic NFTs (#2210)
    * feature: mutable metadata fields for dynamic NFTs
    
    Signed-off-by: svetoslav-nikol0v <svetoslav.nikolov@limechain.tech>
    
    * update: TokenCreateTransaction class and related intergration test
    
    Signed-off-by: svetoslav-nikol0v <svetoslav.nikolov@limechain.tech>
    
    * update: TokenUpdateTransaction class and integration test
    
    Signed-off-by: svetoslav-nikol0v <svetoslav.nikolov@limechain.tech>
    
    * update: TokenInfo class and integration test
    
    Signed-off-by: svetoslav-nikol0v <svetoslav.nikolov@limechain.tech>
    
    * chore: formatting
    
    Signed-off-by: svetoslav-nikol0v <svetoslav.nikolov@limechain.tech>
    
    * chore: formatting
    
    Signed-off-by: svetoslav-nikol0v <svetoslav.nikolov@limechain.tech>
    
    * update: example
    
    Signed-off-by: svetoslav-nikol0v <svetoslav.nikolov@limechain.tech>
    
    * update: integration tests
    
    Signed-off-by: svetoslav-nikol0v <svetoslav.nikolov@limechain.tech>
    
    * update: update examples
    
    Signed-off-by: svetoslav-nikol0v <svetoslav.nikolov@limechain.tech>
    
    * update: adding all integration tests
    
    Signed-off-by: svetoslav-nikol0v <svetoslav.nikolov@limechain.tech>
    
    * update: add tests for fungible token
    
    Signed-off-by: svetoslav-nikol0v <svetoslav.nikolov@limechain.tech>
    
    * update: integration tests
    
    Signed-off-by: svetoslav-nikol0v <svetoslav.nikolov@limechain.tech>
    
    * update: unit tests
    
    Signed-off-by: svetoslav-nikol0v <svetoslav.nikolov@limechain.tech>
    
    * chore: proto version
    
    Signed-off-by: svetoslav-nikol0v <svetoslav.nikolov@limechain.tech>
    
    * update: examples for fungible token
    
    Signed-off-by: svetoslav-nikol0v <svetoslav.nikolov@limechain.tech>
    
    * update: integration tests
    
    Signed-off-by: svetoslav-nikol0v <svetoslav.nikolov@limechain.tech>
    
    * chore: network tag
    
    Signed-off-by: svetoslav-nikol0v <svetoslav.nikolov@limechain.tech>
    
    * chore: formatting
    
    Signed-off-by: svetoslav-nikol0v <svetoslav.nikolov@limechain.tech>
    
    * update: examples
    
    Signed-off-by: svetoslav-nikol0v <svetoslav.nikolov@limechain.tech>
    
    * update: integration tests
    
    Signed-off-by: svetoslav-nikol0v <svetoslav.nikolov@limechain.tech>
    
    * chore: network tag
    
    Signed-off-by: svetoslav-nikol0v <svetoslav.nikolov@limechain.tech>
    
    * chore: network tag
    
    Signed-off-by: svetoslav-nikol0v <svetoslav.nikolov@limechain.tech>
    
    * update: examples
    
    Signed-off-by: svetoslav-nikol0v <svetoslav.nikolov@limechain.tech>
    
    * chore: test change
    
    Signed-off-by: svetoslav-nikol0v <svetoslav.nikolov@limechain.tech>
    
    * chore: update network tag 0.49.0-alpha.5
    
    Signed-off-by: svetoslav-nikol0v <svetoslav.nikolov@limechain.tech>
    
    * update: protobufs
    
    Signed-off-by: svetoslav-nikol0v <svetoslav.nikolov@limechain.tech>
    
    * update: proto package version
    
    Signed-off-by: svetoslav-nikol0v <svetoslav.nikolov@limechain.tech>
    
    * update: examples
    
    Signed-off-by: svetoslav-nikol0v <svetoslav.nikolov@limechain.tech>
    
    * chore: formatting
    
    Signed-off-by: svetoslav-nikol0v <svetoslav.nikolov@limechain.tech>
    
    * update: codeowners
    
    Signed-off-by: svetoslav-nikol0v <svetoslav.nikolov@limechain.tech>
    
    ---------
    
    Signed-off-by: svetoslav-nikol0v <svetoslav.nikolov@limechain.tech>

--- test/unit/TokenUpdateTransaction.js
+++ test/unit/TokenUpdateTransaction.js
@@ -30,6 +30,10 @@ describe("TokenUpdateTransaction", function () {
         const key7 = PrivateKey.fromStringDer(
             "302e020100300506032b657004220420542b4d4a318a1ae5f91071f34c8d900b1150e83d15fe71d22b8581e1203f99ad",
         );
+        const key8 = PrivateKey.fromStringDer(
+            "302e020100300506032b6570042204205447805ce906170817e2bd4e26f4ea1fd5bbc38a2532c7f66b7d7a24f60ee9d5",
+        );
+        const metadata = new Uint8Array([1, 2, 3, 4, 5]);
         const autoRenewAccountId = new AccountId(10);
         const treasuryAccountId = new AccountId(11);
 
@@ -52,6 +56,8 @@ describe("TokenUpdateTransaction", function () {
             .setWipeKey(key5)
             .setSupplyKey(key6)
             .setFeeScheduleKey(key7)
+            .setMetadata(metadata)
+            .setMetadataKey(key8)
             .setNodeAccountIds([new AccountId(4)])
             .setTokenId("0.0.5")
             .setTransactionMemo("random memo")
@@ -95,6 +101,12 @@ describe("TokenUpdateTransaction", function () {
                 },
                 feeScheduleKey: {
                     ed25519: key7.publicKey.toBytesRaw(),
+                },
+                metadata: {
+                    value: metadata,
+                },
+                metadataKey: {
+                    ed25519: key8.publicKey.toBytesRaw(),
                 },
             },
             transactionFee: Long.fromNumber(200000000),
