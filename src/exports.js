2b4b1e72221b0cf9a9d92b42e2b179528cc68d10
svetoslav-nikol0v <svetoslav.nikolov@limechain.tech>
2024-07-18 15:30:54 +0300

    feat: create node transaction (#2402)
    * feat: add addressBook channel
    
    Signed-off-by: svetoslav-nikol0v <svetoslav.nikolov@limechain.tech>
    
    * feat: implement NodeCreateTransaction class
    
    Signed-off-by: svetoslav-nikol0v <svetoslav.nikolov@limechain.tech>
    
    * feat: implement ServiceEndpoint class
    
    Signed-off-by: svetoslav-nikol0v <svetoslav.nikolov@limechain.tech>
    
    * chore: export new classess
    
    Signed-off-by: svetoslav-nikol0v <svetoslav.nikolov@limechain.tech>
    
    * update: proto changes
    
    Signed-off-by: svetoslav-nikol0v <svetoslav.nikolov@limechain.tech>
    
    * update: transaction receipt
    
    Signed-off-by: svetoslav-nikol0v <svetoslav.nikolov@limechain.tech>
    
    * fix: unit tests
    
    Signed-off-by: svetoslav-nikol0v <svetoslav.nikolov@limechain.tech>
    
    * fix: method in node create transaction
    
    Signed-off-by: svetoslav-nikol0v <svetoslav.nikolov@limechain.tech>
    
    ---------
    
    Signed-off-by: svetoslav-nikol0v <svetoslav.nikolov@limechain.tech>

--- src/exports.js
+++ src/exports.js
@@ -182,6 +182,8 @@ export { default as PrecheckStatusError } from "./PrecheckStatusError.js";
 export { default as ReceiptStatusError } from "./ReceiptStatusError.js";
 export { default as LedgerId } from "./LedgerId.js";
 export { default as TokenUpdateNftsTransaction } from "./token/TokenUpdateNftsTransaction.js";
+export { default as NodeCreateTransaction } from "./node/NodeCreateTransaction.js";
+export { default as ServiceEndpoint } from "./node/ServiceEndpoint.js";
 
 /**
  * @typedef {import("./client/Client.js").NetworkName} ClientNetworkName
