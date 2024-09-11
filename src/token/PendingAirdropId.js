f592ba85e1afcbccdb4d18538a9d0a6df52b59d8
ivaylonikolov7 <ivaylo.nikolov@limechain.tech>
2024-09-11 16:16:24 +0300

    feat: Token Claim and Cancel Transaction (#2499)
    * feat: add airdrop claim and cancel transactions
    
    Signed-off-by: Ivaylo Nikolov <ivaylo.nikolov@limechain.tech>
    
    * chore: update protobufs
    
    Signed-off-by: Ivaylo Nikolov <ivaylo.nikolov@limechain.tech>
    
    * feat: update commit messages
    
    Signed-off-by: Ivaylo Nikolov <ivaylo.nikolov@limechain.tech>
    
    * test: add unit tests for cancel and claim
    
    Signed-off-by: Ivaylo Nikolov <ivaylo.nikolov@limechain.tech>
    
    * wip(test): add integration tests for cancel and claim transactions
    
    Signed-off-by: Ivaylo Nikolov <ivaylo.nikolov@limechain.tech>
    
    * fix: claim used the wrong channel function
    
    Signed-off-by: Ivaylo Nikolov <ivaylo.nikolov@limechain.tech>
    
    * refactor: rename transaction name
    
    Signed-off-by: Ivaylo Nikolov <ivaylo.nikolov@limechain.tech>
    
    * refactor: remove claim references in airdropcancel integration test
    
    Signed-off-by: Ivaylo Nikolov <ivaylo.nikolov@limechain.tech>
    
    * test(fix): fix not working test for airdrop cancel and claim
    
    Signed-off-by: Ivaylo Nikolov <ivaylo.nikolov@limechain.tech>
    
    * fix: airdropcancel transaction should work on sender not receiver
    
    Signed-off-by: Ivaylo Nikolov <ivaylo.nikolov@limechain.tech>
    
    * feat: add token airdrop example
    
    Signed-off-by: Ivaylo Nikolov <ivaylo.nikolov@limechain.tech>
    
    * refactor: rename transactions to have the same name as java
    
    Signed-off-by: Ivaylo Nikolov <ivaylo.nikolov@limechain.tech>
    
    * refactor: remove redundant imports and empty lines
    
    Signed-off-by: Ivaylo Nikolov <ivaylo.nikolov@limechain.tech>
    
    * refactor: rename transaction tests  to have the same name as java sdk
    
    Signed-off-by: Ivaylo Nikolov <ivaylo.nikolov@limechain.tech>
    
    * docs: add licenses
    
    Signed-off-by: Ivaylo Nikolov <ivaylo.nikolov@limechain.tech>
    
    * fix: remove dead code from token cancel
    
    Signed-off-by: Ivaylo Nikolov <ivaylo.nikolov@limechain.tech>
    
    * refactor: consistency in constructor
    
    Signed-off-by: Ivaylo Nikolov <ivaylo.nikolov@limechain.tech>
    
    * refactor: change airdrop_supply_per_person naming
    
    Signed-off-by: Ivaylo Nikolov <ivaylo.nikolov@limechain.tech>
    
    * test: fix setting sender id
    
    Signed-off-by: Ivaylo Nikolov <ivaylo.nikolov@limechain.tech>
    
    * refactor: specify what kind of tokens are airdropped
    
    Signed-off-by: Ivaylo Nikolov <ivaylo.nikolov@limechain.tech>
    
    ---------
    
    Signed-off-by: Ivaylo Nikolov <ivaylo.nikolov@limechain.tech>

--- src/token/PendingAirdropId.js
+++ src/token/PendingAirdropId.js
@@ -1,3 +1,23 @@
+/*-
+ * ‌
+ * Hedera JavaScript SDK
+ * ​
+ * Copyright (C) 2020 - 2024 Hedera Hashgraph, LLC
+ * ​
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ‍
+ */
+
 /**
  * @namespace proto
  * @typedef {import("@hashgraph/proto").proto.PendingAirdropId} HashgraphProto.proto.PendingAirdropId
@@ -11,18 +31,27 @@ export default class PendingAirdropId {
     /**
      *
      * @param {object} props
-     * @param {AccountId} props.senderId
-     * @param {AccountId} props.receiverId
+     * @param {AccountId} [props.senderId]
+     * @param {AccountId} [props.receiverId]
      * @param {TokenId?} props.tokenId
      * @param {NftId?} props.nftId
      */
     constructor(props) {
-        this.senderId = props.senderId;
-        this.receiverId = props.receiverId;
+        this._senderId = null;
+        this._receiverId = null;
+        this._tokenId = null;
+        this._nftId = null;
+
+        if (props.receiverId) {
+            this._receiverId = props.receiverId;
+        }
+        if (props.senderId) {
+            this._senderId = props.senderId;
+        }
         if (props.tokenId) {
-            this.tokenId = new TokenId(props.tokenId);
+            this._tokenId = new TokenId(props.tokenId);
         } else if (props.nftId) {
-            this.nftId = new NftId(props.nftId?.tokenId, props.nftId?.serial);
+            this._nftId = new NftId(props.nftId?.tokenId, props.nftId?.serial);
         }
     }
 
@@ -60,14 +89,79 @@ export default class PendingAirdropId {
     }
 
     /**
+     *
+     * @param {AccountId} senderId
+     * @returns
+     */
+    setSenderid(senderId) {
+        this._senderId = senderId;
+        return this;
+    }
+
+    /**
+     * @param {AccountId} receiverId
+     * @returns {this}
+     */
+    setReceiverId(receiverId) {
+        this._receiverId = receiverId;
+        return this;
+    }
+
+    /**
+     * @param {TokenId} tokenId
+     * @returns {this}
+     */
+    setTokenId(tokenId) {
+        this._tokenId = tokenId;
+        return this;
+    }
+
+    /**
+     * @param {NftId} nftId
+     * @returns {this}
+     */
+    setNftId(nftId) {
+        this._nftId = nftId;
+        return this;
+    }
+
+    /**
+     * @returns {?AccountId}
+     */
+    get senderId() {
+        return this._senderId;
+    }
+
+    /**
+     * @returns {?AccountId}
+     */
+    get receiverId() {
+        return this._receiverId;
+    }
+
+    /**
+     * @returns {?TokenId}
+     */
+    get tokenId() {
+        return this._tokenId;
+    }
+
+    /**
+     * @returns {?NftId}
+     */
+    get nftId() {
+        return this._nftId;
+    }
+
+    /**
      *  @returns {HashgraphProto.proto.PendingAirdropId}
      */
     toBytes() {
         return {
-            senderId: this.senderId._toProtobuf(),
-            receiverId: this.receiverId._toProtobuf(),
-            fungibleTokenType: this.tokenId?._toProtobuf(),
-            nonFungibleToken: this.nftId?._toProtobuf(),
+            senderId: this.senderId?._toProtobuf(),
+            receiverId: this._receiverId?._toProtobuf(),
+            fungibleTokenType: this._tokenId?._toProtobuf(),
+            nonFungibleToken: this._nftId?._toProtobuf(),
         };
     }
 }
