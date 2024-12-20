1561164d285570a7b365a612b412507b453588af
Ivan Ivanov <ivanivanov.ii726@gmail.com>
2024-12-20 15:42:32 +0200

    fix: urls
    Signed-off-by: Ivan Ivanov <ivanivanov.ii726@gmail.com>

--- src/query/MirrorNodeContractCallQuery.js
+++ src/query/MirrorNodeContractCallQuery.js
@@ -5,30 +5,28 @@ import MirrorNodeContractQuery from "./MirrorNodeContractQuery.js";
  * @typedef {import("../client/Client.js").default<*, *>} Client
  */
 export default class MirrorNodeContractCallQuery extends MirrorNodeContractQuery {
-    /**
-     * @param {Client} client
-     * @returns {Promise<string>}
-     */
-    async execute(client) {
-        if (this.callData == null) {
-            throw new Error("Call data is required.");
-        }
-
-        const API_ENDPOINT = "contracts/call";
-        const JSON_PAYLOAD = {
-            data: Buffer.from(this.callData).toString("hex"),
-            to: this.contractEvmAddress,
-            estimate: false,
-        };
-
-        /**
-         * @type { { data: { result: string } } }
-         */
-        const mirrorNodeRequest = await this.performMirrorNodeRequest(
-            client,
-            API_ENDPOINT,
-            JSON.stringify(JSON_PAYLOAD),
-        );
-        return mirrorNodeRequest.data.result;
+  /**
+   * @param {Client} client
+   * @returns {Promise<string>}
+   */
+  async execute(client) {
+    if (this.callData == null) {
+      throw new Error("Call data is required.");
     }
+
+    const JSON_PAYLOAD = {
+      data: Buffer.from(this.callData).toString("hex"),
+      to: this.contractEvmAddress,
+      estimate: false,
+    };
+
+    /**
+     * @type { { data: { result: string } } }
+     */
+    const mirrorNodeRequest = await this.performMirrorNodeRequest(
+      client,
+      JSON_PAYLOAD,
+    );
+    return mirrorNodeRequest.data.result;
+  }
 }
