040fc061f610bd5f9776d70c580e20076af25573
Ivaylo Nikolov <ivaylo.nikolov@limechain.tech>
2025-01-06 15:00:57 +0200

    chore: formatting
    Signed-off-by: Ivaylo Nikolov <ivaylo.nikolov@limechain.tech>

--- src/query/MirrorNodeContractCallQuery.js
+++ src/query/MirrorNodeContractCallQuery.js
@@ -5,28 +5,28 @@ import MirrorNodeContractQuery from "./MirrorNodeContractQuery.js";
  * @typedef {import("../client/Client.js").default<*, *>} Client
  */
 export default class MirrorNodeContractCallQuery extends MirrorNodeContractQuery {
-  /**
-   * @param {Client} client
-   * @returns {Promise<string>}
-   */
-  async execute(client) {
-    if (this.callData == null) {
-      throw new Error("Call data is required.");
-    }
-
-    const JSON_PAYLOAD = {
-      data: Buffer.from(this.callData).toString("hex"),
-      to: this.contractEvmAddress,
-      estimate: false,
-    };
-
     /**
-     * @type { { data: { result: string } } }
+     * @param {Client} client
+     * @returns {Promise<string>}
      */
-    const mirrorNodeRequest = await this.performMirrorNodeRequest(
-      client,
-      JSON_PAYLOAD,
-    );
-    return mirrorNodeRequest.data.result;
-  }
+    async execute(client) {
+        if (this.callData == null) {
+            throw new Error("Call data is required.");
+        }
+
+        const JSON_PAYLOAD = {
+            data: Buffer.from(this.callData).toString("hex"),
+            to: this.contractEvmAddress,
+            estimate: false,
+        };
+
+        /**
+         * @type { { data: { result: string } } }
+         */
+        const mirrorNodeRequest = await this.performMirrorNodeRequest(
+            client,
+            JSON_PAYLOAD,
+        );
+        return mirrorNodeRequest.data.result;
+    }
 }
--- src/query/MirrorNodeContractEstimateQuery.js
+++ src/query/MirrorNodeContractEstimateQuery.js
@@ -6,29 +6,29 @@ import MirrorNodeContractQuery from "./MirrorNodeContractQuery.js";
  * @typedef {import("../client/Client.js").default<*, *>} Client
  */
 export default class MirrorNodeContractCallQuery extends MirrorNodeContractQuery {
-  /**
-   * @param {Client} client
-   * @returns {Promise<Long>}
-   */
-  async execute(client) {
-    if (this.callData == null) {
-      throw new Error("Call data is required.");
-    }
-
-    const JSON_PAYLOAD = {
-      data: Buffer.from(this.callData).toString("hex"),
-      from: this.senderEvmAddress,
-      to: this.contractEvmAddress,
-      estimate: true,
-    };
-
     /**
-     * @type {{data: {result: string}}}
+     * @param {Client} client
+     * @returns {Promise<Long>}
      */
-    const mirrorNodeRequest = await this.performMirrorNodeRequest(
-      client,
-      JSON_PAYLOAD,
-    );
-    return Long.fromNumber(Number(mirrorNodeRequest.data.result));
-  }
+    async execute(client) {
+        if (this.callData == null) {
+            throw new Error("Call data is required.");
+        }
+
+        const JSON_PAYLOAD = {
+            data: Buffer.from(this.callData).toString("hex"),
+            from: this.senderEvmAddress,
+            to: this.contractEvmAddress,
+            estimate: true,
+        };
+
+        /**
+         * @type {{data: {result: string}}}
+         */
+        const mirrorNodeRequest = await this.performMirrorNodeRequest(
+            client,
+            JSON_PAYLOAD,
+        );
+        return Long.fromNumber(Number(mirrorNodeRequest.data.result));
+    }
 }
